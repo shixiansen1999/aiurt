c873795d4e2256d0e47aa6821e168ab4ce4cdf5d
lkj <likangjie1221@163.com>
2022-06-23 17:54:04 +0800

    故障分许代码生成

---
+++ aiurt-module-fault/aiurt-module-fault-biz/src/main/java/com/aiurt/modules/faultanalysisreport/service/IFaultAnalysisReportService.java
@@ -0,0 +1,14 @@
+package com.aiurt.modules.faultanalysisreport.service;
+
+import com.aiurt.modules.faultanalysisreport.entity.FaultAnalysisReport;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * @Description: fault_analysis_report
+ * @Author: aiurt
+ * @Date:   2022-06-23
+ * @Version: V1.0
+ */
+public interface IFaultAnalysisReportService extends IService<FaultAnalysisReport> {
+
+}
