49a4b35ed359110249c193792ff4739dabe1924e
zhangwenlong <13797704736@163.com>
2022-06-24 12:19:11 +0800

    创建公共模块实体

---
+++ aiurt-module-inspection/aiurt-module-inspection-biz/src/main/java/com/aiurt/boot/manager/dto/MajorDTO.java
@@ -0,0 +1,27 @@
+package com.aiurt.boot.manager.dto;
+
+
+import com.baomidou.mybatisplus.annotation.TableField;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+/**
+ * @author zwl
+ * @Title:
+ * @Description:
+ * @date 2022/6/2412:15
+ */
+@Data
+public class MajorDTO {
+
+    /**专业编码*/
+    @ApiModelProperty(value = "专业编码")
+    @TableField(exist = false)
+    private String majorCode;
+
+
+    /**专业名称*/
+    @ApiModelProperty(value = "专业名称")
+    @TableField(exist = false)
+    private String majorName;
+}
