20041db3b2b79bc1b2ff8a716a592673db44df97
hlq <1317009531@qq.com>
2022-06-27 10:09:04 +0800

    app巡检任务-任务清单列表查询的实现

--- aiurt-module-patrol/aiurt-module-patrol-biz/src/main/java/com/aiurt/boot/task/controller/PatrolTaskDeviceController.java
+++ aiurt-module-patrol/aiurt-module-patrol-biz/src/main/java/com/aiurt/boot/task/controller/PatrolTaskDeviceController.java
@@ -60,14 +60,14 @@ public class PatrolTaskDeviceController extends BaseController<PatrolTaskDevice,
 	  * app巡检任务-巡检清单列表（巡检工单列表）
 	  * @return
 	  */
-	 @AutoLog(value = "patrol_task- app巡检任务-通过")
-	 @ApiOperation(value="patrol_task- app巡检任务-通过", notes="patrol_task- app巡检任务-通过")
+	 @AutoLog(value = "app巡检任务-巡检清单列表")
+	 @ApiOperation(value="app巡检任务-巡检清单列表", notes="app巡检任务-巡检清单列表")
 	 @GetMapping(value = "/patrolTaskDeviceList")
-	 public Result<Page<PatrolTaskDeviceDTO>>  patrolTaskDeviceList(String id,
+	 public Result<Page<PatrolTaskDeviceDTO>>  patrolTaskDeviceList(PatrolTaskDeviceDTO patrolTaskDeviceDTO,
 																	@RequestParam(name = "pageNo", defaultValue = "1") Integer pageNo,
 																	@RequestParam(name = "pageSize", defaultValue = "10") Integer pageSize, HttpServletRequest req) {
 		 Page<PatrolTaskDeviceDTO> pageList = new Page<PatrolTaskDeviceDTO>(pageNo, pageSize);
-		 pageList = patrolTaskDeviceService.getPatrolTaskDeviceList(pageList, id);
+		 pageList = patrolTaskDeviceService.getPatrolTaskDeviceList(pageList, patrolTaskDeviceDTO);
 		 return Result.OK(pageList);
 	 }
 	/**
