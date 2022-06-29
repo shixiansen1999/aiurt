944fd8f77f97a3d2dac60f543b31e32beeecf6f9
fgw <fugw@aiurt.onaliyun.com>
2022-06-29 09:20:02 +0800

    代码优化

--- aiurt-module-fault/aiurt-module-fault-biz/src/main/java/com/aiurt/modules/fault/controller/FaultRepairRecordController.java
+++ aiurt-module-fault/aiurt-module-fault-biz/src/main/java/com/aiurt/modules/fault/controller/FaultRepairRecordController.java
@@ -5,21 +5,17 @@ import com.aiurt.common.system.base.controller.BaseController;
 import com.aiurt.modules.fault.entity.FaultRepairRecord;
 import com.aiurt.modules.fault.service.IFaultRepairRecordService;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.jeecg.common.api.vo.Result;
-import org.jeecg.common.system.query.QueryGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.util.Arrays;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
  /**
  * @Description: 维修记录
