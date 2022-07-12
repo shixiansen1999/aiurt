54ab8c2b4d6016ddf311b7a5586081c49d7f22f4
hlq <1317009531@qq.com>
2022-07-12 14:27:34 +0800

    巡检代码优化

--- aiurt-module-patrol/aiurt-module-patrol-biz/src/main/java/com/aiurt/boot/task/dto/PatrolTaskDTO.java
+++ aiurt-module-patrol/aiurt-module-patrol-biz/src/main/java/com/aiurt/boot/task/dto/PatrolTaskDTO.java
@@ -130,5 +130,9 @@ public class PatrolTaskDTO
     @Excel(name = "作业类型", width = 15)
     @ApiModelProperty(value = "作业类型")
     private java.lang.String type;
+    /*** 任务计划执行日期范围*/
+    @Excel(name = "任务计划执行日期范围", width = 15)
+    @ApiModelProperty(value = "任务计划执行日期范围")
+    private String dateScope;
 }
 
