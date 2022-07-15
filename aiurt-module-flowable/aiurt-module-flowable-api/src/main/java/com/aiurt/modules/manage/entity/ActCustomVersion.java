fa85c04e7eb86165008e776ce21069471960ffd4
fgw <fugw@aiurt.onaliyun.com>
2022-07-15 11:26:20 +0800

    flowable版本管理

---
+++ aiurt-module-flowable/aiurt-module-flowable-api/src/main/java/com/aiurt/modules/manage/entity/ActCustomVersion.java
@@ -0,0 +1,85 @@
+package com.aiurt.modules.manage.entity;
+
+import java.io.Serializable;
+import java.io.UnsupportedEncodingException;
+import java.util.Date;
+import java.math.BigDecimal;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.jeecgframework.poi.excel.annotation.Excel;
+import com.aiurt.common.aspect.annotation.Dict;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+/**
+ * @Description: 版本管理
+ * @Author: aiurt
+ * @Date:   2022-07-15
+ * @Version: V1.0
+ */
+@Data
+@TableName("act_custom_version")
+@Accessors(chain = true)
+@EqualsAndHashCode(callSuper = false)
+@ApiModel(value="act_custom_version对象", description="流程版本管理")
+public class ActCustomVersion implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+	/**主键*/
+	@TableId(type = IdType.ASSIGN_ID)
+    @ApiModelProperty(value = "主键")
+    private String id;
+
+	/**创建人*/
+    @ApiModelProperty(value = "创建人")
+    private String createBy;
+
+	/**创建日期*/
+	@JsonFormat(timezone = "GMT+8",pattern = "yyyy-MM-dd HH:mm:ss")
+    @DateTimeFormat(pattern="yyyy-MM-dd HH:mm:ss")
+    @ApiModelProperty(value = "创建日期")
+    private Date createTime;
+
+	/**更新人*/
+    @ApiModelProperty(value = "更新人")
+    private String updateBy;
+
+	/**更新日期*/
+	@JsonFormat(timezone = "GMT+8",pattern = "yyyy-MM-dd HH:mm:ss")
+    @DateTimeFormat(pattern="yyyy-MM-dd HH:mm:ss")
+    @ApiModelProperty(value = "更新日期")
+    private Date updateTime;
+
+	/**所属部门*/
+    @ApiModelProperty(value = "所属部门")
+    private String sysOrgCode;
+
+	/**流程引擎的定义Id*/
+    @ApiModelProperty(value = "流程引擎的定义Id")
+    private String processDefinitionId;
+
+	/**流程引擎的部署Id*/
+    @ApiModelProperty(value = "流程引擎的部署Id")
+    private String deployId;
+
+	/**流程Id(流程模型)*/
+    @ApiModelProperty(value = "流程Id(流程模型)")
+    private String modelId;
+
+	/**是否为主版本(1是,0否)*/
+    @ApiModelProperty(value = "是否为主版本(1是,0否)")
+    private String mainVersion;
+
+	/**发布时间*/
+	@Excel(name = "发布时间", width = 20, format = "yyyy-MM-dd HH:mm:ss")
+	@JsonFormat(timezone = "GMT+8",pattern = "yyyy-MM-dd HH:mm:ss")
+    @DateTimeFormat(pattern="yyyy-MM-dd HH:mm:ss")
+    @ApiModelProperty(value = "发布时间")
+    private Date deployTime;
+}
---
+++ aiurt-module-flowable/aiurt-module-flowable-biz/src/main/java/com/aiurt/modules/manage/mapper/ActCustomVersionMapper.java
@@ -0,0 +1,15 @@
+package com.aiurt.modules.manage.mapper;
+
+
+import com.aiurt.modules.manage.entity.ActCustomVersion;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * @Description: 版本管理
+ * @Author: aiurt
+ * @Date:   2022-07-15
+ * @Version: V1.0
+ */
+public interface ActCustomVersionMapper extends BaseMapper<ActCustomVersion> {
+
+}
