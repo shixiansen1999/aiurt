43ba25e8fc9dc9ccf50c8c53c92f786eccfe97a1
fgw <fugw@aiurt.onaliyun.com>
2022-07-27 18:01:31 +0800

    redis 全局缓存key增加平台标识

--- aiurt-base/aiurt-base-tools/src/main/java/com/aiurt/common/constant/CacheConstant.java
+++ aiurt-base/aiurt-base-tools/src/main/java/com/aiurt/common/constant/CacheConstant.java
@@ -10,54 +10,54 @@ public interface CacheConstant {
 	/**
 	 * 字典信息缓存（含禁用的字典项）
 	 */
-    public static final String SYS_DICT_CACHE = "sys:cache:dict";
+    public static final String SYS_DICT_CACHE = "platform:sys:cache:dict";
 
 	/**
 	 * 字典信息缓存 status为有效的
 	 */
-	public static final String SYS_ENABLE_DICT_CACHE = "sys:cache:dictEnable";
+	public static final String SYS_ENABLE_DICT_CACHE = "platform:sys:cache:dictEnable";
 	/**
 	 * 表字典信息缓存
 	 */
-    public static final String SYS_DICT_TABLE_CACHE = "sys:cache:dictTable";
+    public static final String SYS_DICT_TABLE_CACHE = "platform:sys:cache:dictTable";
 	public static final String SYS_DICT_TABLE_BY_KEYS_CACHE = SYS_DICT_TABLE_CACHE + "ByKeys";
 
 	/**
 	 * 数据权限配置缓存
 	 */
-    public static final String SYS_DATA_PERMISSIONS_CACHE = "sys:cache:permission:datarules";
+    public static final String SYS_DATA_PERMISSIONS_CACHE = "platform:sys:cache:permission:datarules";
 
 	/**
 	 * 缓存用户信息
 	 */
-	public static final String SYS_USERS_CACHE = "sys:cache:user";
+	public static final String SYS_USERS_CACHE = "platform:sys:cache:user";
 
 	/**
 	 * 全部部门信息缓存
 	 */
-	public static final String SYS_DEPARTS_CACHE = "sys:cache:depart:alldata";
+	public static final String SYS_DEPARTS_CACHE = "platform:sys:cache:depart:alldata";
 
 
 	/**
 	 * 全部部门ids缓存
 	 */
-	public static final String SYS_DEPART_IDS_CACHE = "sys:cache:depart:allids";
+	public static final String SYS_DEPART_IDS_CACHE = "platform:sys:cache:depart:allids";
 
 
 	/**
 	 * 测试缓存key
 	 */
-	public static final String TEST_DEMO_CACHE = "test:demo";
+	public static final String TEST_DEMO_CACHE = "platform:test:demo";
 
 	/**
 	 * 字典信息缓存
 	 */
-	public static final String SYS_DYNAMICDB_CACHE = "sys:cache:dbconnect:dynamic:";
+	public static final String SYS_DYNAMICDB_CACHE = "platform:sys:cache:dbconnect:dynamic:";
 
 	/**
 	 * gateway路由缓存
 	 */
-	public static final String GATEWAY_ROUTES = "sys:cache:cloud:gateway_routes";
+	public static final String GATEWAY_ROUTES = "platform:sys:cache:cloud:gateway_routes";
 
 
 	/**
@@ -69,35 +69,35 @@ public interface CacheConstant {
 	 * TODO 冗余代码 待删除
 	 *插件商城排行榜
 	 */
-	public static final String PLUGIN_MALL_RANKING = "pluginMall::rankingList";
+	public static final String PLUGIN_MALL_RANKING = "platform:pluginMall::rankingList";
 	/**
 	 * TODO 冗余代码 待删除
 	 *插件商城排行榜
 	 */
-	public static final String PLUGIN_MALL_PAGE_LIST = "pluginMall::queryPageList";
+	public static final String PLUGIN_MALL_PAGE_LIST = "platform:pluginMall::queryPageList";
 
 
 	/**
 	 * online列表页配置信息缓存key
 	 */
-	public static final String ONLINE_LIST = "sys:cache:online:list";
+	public static final String ONLINE_LIST = "platform:sys:cache:online:list";
 
 	/**
 	 * online表单页配置信息缓存key
 	 */
-	public static final String ONLINE_FORM = "sys:cache:online:form";
+	public static final String ONLINE_FORM = "platform:sys:cache:online:form";
 
 	/**
 	 * online报表
 	 */
-	public static final String ONLINE_RP = "sys:cache:online:rp";
+	public static final String ONLINE_RP = "platform:sys:cache:online:rp";
 
 	/**
 	 * online图表
 	 */
-	public static final String ONLINE_GRAPH = "sys:cache:online:graph";
+	public static final String ONLINE_GRAPH = "platform:sys:cache:online:graph";
 	/**
 	 * 拖拽页面信息缓存
 	 */
-	public static final String DRAG_PAGE_CACHE = "drag:cache:page";
+	public static final String DRAG_PAGE_CACHE = "platform:drag:cache:page";
 }
