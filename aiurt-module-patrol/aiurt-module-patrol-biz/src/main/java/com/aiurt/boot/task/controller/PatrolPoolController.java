b11992464b95f8b5a0b1f0b6d90ac5a9b0cf0e6b
cjb <chenjiabao@aiurt.onaliyun.com>
2022-08-04 09:51:23 +0800

    巡检模块操作日志规范优化

--- aiurt-module-patrol/aiurt-module-patrol-biz/src/main/java/com/aiurt/boot/task/controller/PatrolPoolController.java
+++ aiurt-module-patrol/aiurt-module-patrol-biz/src/main/java/com/aiurt/boot/task/controller/PatrolPoolController.java
@@ -2,6 +2,7 @@ package com.aiurt.boot.task.controller;
 
 import com.aiurt.boot.pool.PatrolTaskMissingDetection;
 import com.aiurt.boot.pool.TaskPool;
+import com.aiurt.common.aspect.annotation.AutoLog;
 import org.jeecg.common.api.vo.Result;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +20,7 @@ public class PatrolPoolController {
     /**
      * 定时生成巡检任务触发接口(供测试用)
      */
+    @AutoLog(value = "定时生成巡检任务触发接口(供测试用)", operateType = 2, operateTypeAlias = "新增", permissionUrl = "/taskpool")
     @RequestMapping(value = "/taskpool", method = RequestMethod.POST)
     public Result<?> taskPool() {
         pool.execute();
@@ -30,6 +32,7 @@ public class PatrolPoolController {
      *
      * @return
      */
+    @AutoLog(value = "漏检任务检测触发接口(供测试用)", operateType = 3, operateTypeAlias = "修改", permissionUrl = "/taskmiss")
     @RequestMapping(value = "/taskmiss", method = RequestMethod.POST)
     public Result<?> taskMissDetection() {
         taskMissingDetection.execute();
