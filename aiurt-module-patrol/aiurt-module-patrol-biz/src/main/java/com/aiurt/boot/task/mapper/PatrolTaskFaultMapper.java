b1a30dc38171490f254556b56be305b4fefac4ad
cjb <chenjiabao@aiurt.onaliyun.com>
2022-08-08 09:30:19 +0800

    巡检单故障上报entity、mapper、service

---
+++ aiurt-module-patrol/aiurt-module-patrol-biz/src/main/java/com/aiurt/boot/task/mapper/PatrolTaskFaultMapper.java
@@ -0,0 +1,14 @@
+package com.aiurt.boot.task.mapper;
+
+import com.aiurt.boot.task.entity.PatrolTaskFault;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * @Description: patrol_task_fault
+ * @Author: aiurt
+ * @Date:   2022-08-08
+ * @Version: V1.0
+ */
+public interface PatrolTaskFaultMapper extends BaseMapper<PatrolTaskFault> {
+
+}
---
+++ aiurt-module-patrol/aiurt-module-patrol-biz/src/main/java/com/aiurt/boot/task/service/IPatrolTaskFaultService.java
@@ -0,0 +1,14 @@
+package com.aiurt.boot.task.service;
+
+import com.aiurt.boot.task.entity.PatrolTaskFault;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * @Description: patrol_task_fault
+ * @Author: aiurt
+ * @Date:   2022-08-08
+ * @Version: V1.0
+ */
+public interface IPatrolTaskFaultService extends IService<PatrolTaskFault> {
+
+}
