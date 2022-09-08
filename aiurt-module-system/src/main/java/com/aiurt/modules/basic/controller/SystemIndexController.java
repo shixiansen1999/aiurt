4d4dd250044bea56c47bf14cc6c4158a56a8e98f
fgw <fugw@aiurt.onaliyun.com>
2022-09-08 16:34:22 +0800

    首页获取天气

--- aiurt-module-system/src/main/java/com/aiurt/modules/basic/controller/SystemIndexController.java
+++ aiurt-module-system/src/main/java/com/aiurt/modules/basic/controller/SystemIndexController.java
@@ -3,6 +3,7 @@ package com.aiurt.modules.basic.controller;
 import com.aiurt.modules.basic.dto.WeatherDetailDTO;
 import com.aiurt.modules.basic.service.ISystemIndexService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.jeecg.common.api.vo.Result;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,6 +23,7 @@ public class SystemIndexController {
     @Autowired
     private ISystemIndexService systemIndexService;
 
+    @ApiOperation("获取天气信息")
     @GetMapping("/getWeatherInfo")
     public Result<WeatherDetailDTO> getWeatherInfo() {
         WeatherDetailDTO weatherDetailDTO = systemIndexService.getWeatherInfo();
