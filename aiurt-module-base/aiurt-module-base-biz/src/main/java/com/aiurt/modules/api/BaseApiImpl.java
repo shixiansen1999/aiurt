4a46cc8d97b29b5511dabfe34e59868f26530e80
qkx <572657543@qq.com>
2022-09-09 11:46:41 +0800

    首页日常统计

--- aiurt-module-base/aiurt-module-base-biz/src/main/java/com/aiurt/modules/api/BaseApiImpl.java
+++ aiurt-module-base/aiurt-module-base-biz/src/main/java/com/aiurt/modules/api/BaseApiImpl.java
@@ -1,6 +1,8 @@
 package com.aiurt.modules.api;
 
 import com.aiurt.modules.dailyschedule.entity.DailySchedule;
+import com.aiurt.modules.dailyschedule.service.IDailyScheduleService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -9,10 +11,13 @@ import java.util.Map;
 @Service
 public class BaseApiImpl implements IBaseApi{
 
+    @Autowired
+    private IDailyScheduleService dailyScheduleService;
+
     @Override
     public Map<String, List<DailySchedule>> queryDailyScheduleList(Integer year, Integer month) {
 
 
-        return null;
+        return dailyScheduleService.queryDailyScheduleList(year,month);
     }
 }
