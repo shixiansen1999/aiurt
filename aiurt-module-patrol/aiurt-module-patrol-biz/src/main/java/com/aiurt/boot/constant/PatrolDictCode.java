c48ca397132bf416595dcb243b92f633aac37354
cjb <chenjiabao@aiurt.onaliyun.com>
2022-09-14 18:27:08 +0800

    大屏巡视模块巡视数据统计

---
+++ aiurt-module-patrol/aiurt-module-patrol-biz/src/main/java/com/aiurt/boot/constant/PatrolDictCode.java
@@ -0,0 +1,19 @@
+package com.aiurt.boot.constant;
+
+/**
+ * 巡视模块字典code
+ */
+public interface PatrolDictCode {
+    /**
+     * 巡视任务状态字典编码
+     */
+    String TASK_STATUS = "patrol_task_status";
+    /**
+     * 巡视任务漏检状态字典编码
+     */
+    String OMIT_STATUS = "patrol_omit_status";
+    /**
+     * 巡视任务异常状态字典编码
+     */
+    String ABNORMAL_STATE = "patrol_abnormal_state";
+}
