e37aa1112252a23c7a4e508543468bd0eaee4ad5
hlq <1317009531@qq.com>
2022-09-22 15:48:45 +0800

    计算巡检的漏巡视数（上周、时间查询范围内）

--- aiurt-module-patrol/aiurt-module-patrol-biz/src/main/java/com/aiurt/boot/report/model/PatrolReport.java
+++ aiurt-module-patrol/aiurt-module-patrol-biz/src/main/java/com/aiurt/boot/report/model/PatrolReport.java
@@ -1,5 +1,6 @@
 package com.aiurt.boot.report.model;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -29,55 +30,66 @@ public class PatrolReport {
     private String taskCode;/**
      * 班组
      */
+    @ApiModelProperty("班组名")
     @Excel(name = "班组", width = 15)
     private String orgName;
     /**
      * 班组code
      */
+
     private String orgCode;
     /**
      * 巡视任务总数
      */
+    @ApiModelProperty("巡视任务总数")
     @Excel(name = "巡视任务总数", width = 15)
     private Integer taskTotal;
     /**
      * 已巡视数
      */
+    @ApiModelProperty("已巡视数")
     @Excel(name = "已巡视数", width = 15)
     private Integer inspectedNumber;
     /**
      * 未巡视数
      */
+    @ApiModelProperty("未巡视数")
     @Excel(name = "未巡视数", width = 15)
     private Integer notInspectedNumber;
     /**
      * 漏巡视数
      */
+    @ApiModelProperty("漏巡视数")
     @Excel(name = "漏巡视数", width = 15)
     private Integer missInspectedNumber;
     /**
      * 平均每周漏巡视数
      */
+    @ApiModelProperty("平均每周漏巡视数")
     @Excel(name = "平均每周漏巡视数", width = 15)
-    private Integer awmPatrolNumber;
+    private String awmPatrolNumber;
     /**
      * 平均每月漏巡视数
      */
+    @ApiModelProperty("平均每月漏巡视数")
     @Excel(name = "平均每月漏巡视数", width = 15)
-    private Integer ammPatrolNumber;
+    private String ammPatrolNumber;
     /**
      * 完成率
      */
+    @ApiModelProperty("完成率")
     @Excel(name = "完成率", width = 15)
     private String completionRate;
     /**
      * 异常数量
      */
+    @ApiModelProperty("异常数量")
     @Excel(name = "异常数量", width = 15)
     private Integer abnormalNumber;
     /**
      * 故障数量
      */
+    @ApiModelProperty("故障数量")
     @Excel(name = "故障数量", width = 15)
     private Integer faultNumber;
 }
