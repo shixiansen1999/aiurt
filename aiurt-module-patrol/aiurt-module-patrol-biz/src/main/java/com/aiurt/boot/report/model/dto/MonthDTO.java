181318691ad964eefa5d544bfd9caf127669d051
huangqiyao <1667981208@qq.com>
2022-09-28 09:24:17 +0800

    代码优化

--- aiurt-module-patrol/aiurt-module-patrol-biz/src/main/java/com/aiurt/boot/report/model/dto/MonthDTO.java
+++ aiurt-module-patrol/aiurt-module-patrol-biz/src/main/java/com/aiurt/boot/report/model/dto/MonthDTO.java
@@ -1,5 +1,6 @@
 package com.aiurt.boot.report.model.dto;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 /**
@@ -15,17 +16,29 @@ import lombok.Data;
 public class MonthDTO {
     private String orgName;
     private String systemName;
+    @ApiModelProperty(value = "一月")
     private Integer january;
+    @ApiModelProperty(value = "二月")
     private Integer february;
+    @ApiModelProperty(value = "三月")
     private Integer march;
+    @ApiModelProperty(value = "四月")
     private Integer april;
+    @ApiModelProperty(value = "五月")
     private Integer may;
+    @ApiModelProperty(value = "六月")
     private Integer june;
+    @ApiModelProperty(value = "七月")
     private Integer july;
+    @ApiModelProperty(value = "八月")
     private Integer august;
+    @ApiModelProperty(value = "九月")
     private Integer september;
+    @ApiModelProperty(value = "十月")
     private Integer october;
+    @ApiModelProperty(value = "十一月")
     private Integer november;
+    @ApiModelProperty(value = "十二月")
     private Integer december;
 
 }
