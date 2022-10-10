bed4edaf43866bc8cee8373bc758c299f0e8b032
fgw <fugw@aiurt.onaliyun.com>
2022-10-10 11:51:08 +0800

    docs(泛微) 增加注释

--- aiurt-module-system/src/main/java/com/aiurt/modules/weaver/service/IWeaverSSOService.java
+++ aiurt-module-system/src/main/java/com/aiurt/modules/weaver/service/IWeaverSSOService.java
@@ -6,7 +6,16 @@ import com.aiurt.modules.weaver.service.entity.WeaverSsoRestultDTO;
  * @author fgw
  */
 public interface IWeaverSSOService {
+
+    /**
+     * 获取token， header
+     * @return
+     */
     public WeaverSsoRestultDTO getToken();
 
+    /**
+     * 获取ssotoken
+     * @return
+     */
     public String ssoToken();
 }
