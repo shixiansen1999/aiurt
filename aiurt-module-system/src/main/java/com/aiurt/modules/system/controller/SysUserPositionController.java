daf337f6d4afad5a097c488b62d5546d91cb172a
hlq <1317009531@qq.com>
2022-10-31 11:09:49 +0800

    feat(GPS、WIFI定位数据上报): 保存接口的定义及实现

---
+++ aiurt-module-system/src/main/java/com/aiurt/modules/system/controller/SysUserPositionController.java
@@ -0,0 +1,54 @@
+package com.aiurt.modules.system.controller;
+
+import com.aiurt.common.aspect.annotation.AutoLog;
+import com.aiurt.modules.system.entity.SysUserPosition;
+import com.aiurt.modules.system.service.ISysUserPositionService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.jeecg.common.api.vo.Result;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+/**
+ * GPS、wifi定位数据上报
+ */
+@Slf4j
+@Api(tags = "GPS、wifi定位数据上报")
+@RestController
+@RequestMapping("/sys/user/position")
+public class SysUserPositionController {
+
+    @Autowired
+    private ISysUserPositionService sysUserPositionService;
+
+
+
+    /**
+     * 添加
+     *
+     * @param sysUserPosition
+     * @return
+     */
+    @AutoLog(value = "GPS、wifi定位数据上报")
+    @ApiOperation(value = "GPS、wifi定位数据上报", notes = "GPS、wifi定位数据上报")
+    @PostMapping(value = "/add")
+    public Result<SysUserPosition> add(@RequestBody SysUserPosition sysUserPosition) {
+        Result<SysUserPosition> result = new Result<SysUserPosition>();
+        try {
+            sysUserPosition.setUploadTime(new Date());
+            sysUserPositionService.save(sysUserPosition);
+            result.success("添加成功！");
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+            result.error500("操作失败");
+        }
+        return result;
+    }
+
+}
---
+++ aiurt-module-system/src/main/java/com/aiurt/modules/system/mapper/SysUserPositionMapper.java
@@ -0,0 +1,11 @@
+package com.aiurt.modules.system.mapper;
+
+import com.aiurt.modules.system.entity.SysUserPosition;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * GPS、wifi定位数据上报
+ */
+public interface SysUserPositionMapper extends BaseMapper<SysUserPosition> {
+
+}
