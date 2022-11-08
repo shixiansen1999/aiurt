b9a0360e59285b7bc46b0cdfbb782ce50ff3f89d
huangqiyao <1667981208@qq.com>
2022-11-08 17:36:46 +0800

    fix: 导出模板遗留的问题

--- aiurt-module-system/src/main/java/com/aiurt/modules/material/controller/MaterialBaseController.java
+++ aiurt-module-system/src/main/java/com/aiurt/modules/material/controller/MaterialBaseController.java
@@ -425,7 +425,7 @@ public class MaterialBaseController {
     @ApiOperation(value = "下载物资导入模板", notes = "下载物资导入模板")
     @RequestMapping(value = "/downloadExcel", method = RequestMethod.GET)
     public void downloadExcel(HttpServletResponse response, HttpServletRequest request) throws IOException {
-        ClassPathResource classPathResource =  new ClassPathResource("template/materialBase.xlsx");
+        ClassPathResource classPathResource =  new ClassPathResource("templates/materialBase.xlsx");
         InputStream bis = classPathResource.getInputStream();
         BufferedOutputStream out = new BufferedOutputStream(response.getOutputStream());
         int len = 0;
