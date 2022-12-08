1d5e0b4115f6c88dae59367e94ae6777e08fee98
lkj <likangjie1221@163.com>
2022-12-08 17:53:27 +0800

    fix(设备主数据导入组件为空情况判断):

--- aiurt-module-system/src/main/java/com/aiurt/modules/device/service/impl/DeviceServiceImpl.java
+++ aiurt-module-system/src/main/java/com/aiurt/modules/device/service/impl/DeviceServiceImpl.java
@@ -80,6 +80,7 @@ import org.springframework.web.multipart.MultipartHttpServletRequest;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.*;
+import java.lang.reflect.Field;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.*;
