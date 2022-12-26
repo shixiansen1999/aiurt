d0f33c50785d06d37b1b18e78d617d7f51a0fcf1
zhangwenlon <longlong20001208@163.com>
2022-12-26 16:53:34 +0800

    fix(规程规范与知识库)：pc文档树的权限优化

--- aiurt-module-base/aiurt-module-base-api/src/main/java/com/aiurt/modules/sysfile/vo/SysFileTypeTreeVO.java
+++ aiurt-module-base/aiurt-module-base-api/src/main/java/com/aiurt/modules/sysfile/vo/SysFileTypeTreeVO.java
@@ -1,12 +1,14 @@
 package com.aiurt.modules.sysfile.vo;
 
 import com.aiurt.modules.sysfile.entity.SysFileType;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
 
 import java.io.Serializable;
 import java.util.List;
+import java.util.Set;
 
 /**
  * 树状返回
@@ -23,6 +25,15 @@ public class SysFileTypeTreeVO extends SysFileType implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	private List<SysFileTypeTreeVO> children;
+
+	@ApiModelProperty("可编辑")
+	private Set<SimpUserVO> editUsers;
+
+	@ApiModelProperty("可上传")
+	private Set<SimpUserVO> uploadStatus;
+
+	@ApiModelProperty("可删除")
+	private Set<SimpUserVO> deleteStatus;
 
 
 }
