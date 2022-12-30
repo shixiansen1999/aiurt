44bbb604212fe4c83fca7e307f539b314ac13af1
zhangwenlong <13797704736@163.com>
2022-12-30 16:51:31 +0800

    fix(应急物资管理): 应急物资巡检，应急物资台账查看

--- aiurt-module-emergency/aiurt-module-emergency-biz/src/main/java/com/aiurt/boot/materials/service/impl/EmergencyMaterialsInvoicesItemServiceImpl.java
+++ aiurt-module-emergency/aiurt-module-emergency-biz/src/main/java/com/aiurt/boot/materials/service/impl/EmergencyMaterialsInvoicesItemServiceImpl.java
@@ -1,5 +1,6 @@
 package com.aiurt.boot.materials.service.impl;
 
+import cn.hutool.core.collection.CollUtil;
 import cn.hutool.core.util.StrUtil;
 import com.aiurt.boot.materials.mapper.EmergencyMaterialsInvoicesItemMapper;
 import com.aiurt.boot.materials.service.IEmergencyMaterialsInvoicesItemService;
@@ -13,6 +14,7 @@ import org.springframework.stereotype.Service;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @Description: emergency_materials_invoices_item
@@ -34,10 +36,14 @@ public class EmergencyMaterialsInvoicesItemServiceImpl extends ServiceImpl<Emerg
         //父级
         List<EmergencyMaterialsInvoicesItem> patrolRecord = emergencyMaterialsInvoicesItemMapper.getPatrolRecord(pageList,materialsCode, startTime, endTime,standardCode,"0");
         patrolRecord.forEach(e->{
-            if (StrUtil.isNotBlank(e.getPatrolId())){
-                //根据巡检人id查询巡检名称
-                LoginUser userById = iSysBaseAPI.getUserById(e.getPatrolId());
-                e.setPatrolName(userById.getRealname());
+            if (StrUtil.isNotBlank(e.getPatrolId())) {
+                //根据巡视人id查询巡视人名称
+                String[] split = e.getPatrolId().split(",");
+                List<LoginUser> loginUsers = iSysBaseAPI.queryAllUserByIds(split);
+                if (CollUtil.isNotEmpty(loginUsers)){
+                    String collect = loginUsers.stream().map(LoginUser::getRealname).collect(Collectors.joining(","));
+                    e.setPatrolName(collect);
+                }
             }if(StrUtil.isNotBlank(e.getPatrolTeamCode())){
                 //根据巡检班组code查询巡检班组名称
                 String departNameByOrgCode = iSysBaseAPI.getDepartNameByOrgCode(e.getPatrolTeamCode());
