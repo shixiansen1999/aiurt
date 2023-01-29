8ec73ee538dac9dcf57ff149be1d8c78bc2c68da
huangqiyao <1667981208@qq.com>
2023-01-29 11:38:15 +0800

    fix(盘点固定资产):修改

--- aiurt-module-asset/aiurt-module-asset-biz/src/main/java/com/aiurt/boot/record/service/impl/FixedAssetsCheckRecordServiceImpl.java
+++ aiurt-module-asset/aiurt-module-asset-biz/src/main/java/com/aiurt/boot/record/service/impl/FixedAssetsCheckRecordServiceImpl.java
@@ -125,7 +125,6 @@ public class FixedAssetsCheckRecordServiceImpl extends ServiceImpl<FixedAssetsCh
             Calendar calendar =  Calendar.getInstance();
             FixedAssets fixedAssets = fixedAssetsService.lambdaQuery()
                     .eq(FixedAssets::getAssetCode,record.getAssetCode())
-                    .eq(FixedAssets::getStatus,FixedAssetsConstant.STATUS_1)
                     .eq(FixedAssets::getDelFlag,FixedAssetsConstant.STATUS_0).one();
             SimpleDateFormat format = new SimpleDateFormat("yyyy");
             record.setUsefulLife(calendar.get(Calendar.YEAR)- Integer.valueOf(format.format(fixedAssets.getStartDate())));
