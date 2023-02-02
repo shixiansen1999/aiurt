8e76e67f3b8c7adcbbc8cf6f400b9a702ad650c0
zhangwenlong <13797704736@163.com>
2023-02-02 19:45:05 +0800

    fix(应急物资管理-应急物资使用): 组织机构树查询bug修改

--- aiurt-module-emergency/aiurt-module-emergency-biz/src/main/java/com/aiurt/boot/materials/service/impl/EmergencyMaterialsUsageServiceImpl.java
+++ aiurt-module-emergency/aiurt-module-emergency-biz/src/main/java/com/aiurt/boot/materials/service/impl/EmergencyMaterialsUsageServiceImpl.java
@@ -1,21 +1,27 @@
 package com.aiurt.boot.materials.service.impl;
 
+import cn.hutool.core.collection.CollectionUtil;
 import cn.hutool.core.util.StrUtil;
 import com.aiurt.boot.materials.entity.EmergencyMaterialsCategory;
 import com.aiurt.boot.materials.mapper.EmergencyMaterialsCategoryMapper;
 import com.aiurt.boot.materials.mapper.EmergencyMaterialsUsageMapper;
 import com.aiurt.boot.materials.service.IEmergencyMaterialsUsageService;
 import com.aiurt.boot.materials.entity.EmergencyMaterialsUsage;
+import com.aiurt.common.api.CommonAPI;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import org.apache.shiro.SecurityUtils;
 import org.jeecg.common.system.api.ISysBaseAPI;
+import org.jeecg.common.system.vo.CsUserDepartModel;
 import org.jeecg.common.system.vo.LoginUser;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @Description: emergency_materials_usage
@@ -35,8 +41,20 @@ public class EmergencyMaterialsUsageServiceImpl extends ServiceImpl<EmergencyMat
     @Autowired
     private ISysBaseAPI iSysBaseAPI;
 
+    @Lazy
+    @Autowired
+    private CommonAPI api;
+
     @Override
     public Page<EmergencyMaterialsUsage> getUsageRecordList(Page<EmergencyMaterialsUsage> pageList, EmergencyMaterialsUsage condition) {
+        LoginUser sysUser = (LoginUser) SecurityUtils.getSubject().getPrincipal();
+        List<CsUserDepartModel> departByUserId = api.getDepartByUserId(sysUser.getId());
+        if(StrUtil.isBlank(condition.getPrimaryOrg()) && CollectionUtil.isNotEmpty(departByUserId)){
+            List<String> collect = departByUserId.stream().map(CsUserDepartModel::getOrgCode).collect(Collectors.toList());
+            if (CollectionUtil.isNotEmpty(collect)){
+                condition.setPrimaryCodeList(collect);
+            }
+        }
         List<EmergencyMaterialsUsage> usageRecordList = emergencyMaterialsUsageMapper.getUsageRecordList(pageList, condition);
         usageRecordList.forEach(e->{
             //根据使用人ID查询使用人名称
