0db7e5bb1144ae2acb52fc713106d1ad34987a24
huangqiyao <1667981208@qq.com>
2023-02-02 11:41:38 +0800

    fix(固定资产):增加发送消息操作 修改查询显示问题
    (cherry picked from commit a4922cfbd2e3060cdaac0649911dac16a64e194c)

--- aiurt-module-asset/aiurt-module-asset-biz/src/main/java/com/aiurt/boot/check/service/impl/FixedAssetsCheckServiceImpl.java
+++ aiurt-module-asset/aiurt-module-asset-biz/src/main/java/com/aiurt/boot/check/service/impl/FixedAssetsCheckServiceImpl.java
@@ -24,8 +24,10 @@ import com.aiurt.boot.check.vo.FixedAssetsCheckVO;
 import com.aiurt.boot.constant.FixedAssetsConstant;
 import com.aiurt.boot.record.entity.FixedAssetsCheckRecord;
 import com.aiurt.boot.record.service.IFixedAssetsCheckRecordService;
+import com.aiurt.common.api.dto.message.BusMessageDTO;
 import com.aiurt.common.constant.CommonConstant;
 import com.aiurt.common.exception.AiurtBootException;
+import com.aiurt.common.util.SysAnnmentTypeEnum;
 import com.aiurt.modules.common.api.IFlowableBaseUpdateStatusService;
 import com.aiurt.modules.common.entity.RejectFirstUserTaskEntity;
 import com.aiurt.modules.common.entity.UpdateStateEntity;
@@ -250,6 +252,21 @@ public class FixedAssetsCheckServiceImpl extends ServiceImpl<FixedAssetsCheckMap
     public String saveCheckInfo(FixedAssetsCheck fixedAssetsCheck) {
         this.save(fixedAssetsCheck);
         this.saveOrgAndCategoryCode(fixedAssetsCheck);
+        // 发消息
+        BusMessageDTO messageDTO = new BusMessageDTO();
+        //设置消息属性
+        messageDTO.setStartTime(new Date());
+        messageDTO.setEndTime(new Date());
+        messageDTO.setPriority("H");
+        messageDTO.setCategory("1");
+        messageDTO.setTitle("固定资产盘点人通知");
+        messageDTO.setBusType(SysAnnmentTypeEnum.ASSET_CHECKER.getType());
+        messageDTO.setToAll(false);
+        LoginUser userById = sysBaseApi.getUserById(fixedAssetsCheck.getCheckId());
+        messageDTO.setContent(String.format("%s你好，您作为%s盘点任务的盘点人,尽快完成!", userById.getRealname(), fixedAssetsCheck.getInventoryList()));
+        //设置接收人
+        messageDTO.setToUser(userById.getUsername());
+        sysBaseApi.sendBusAnnouncement(messageDTO);
         return fixedAssetsCheck.getId();
     }
 
