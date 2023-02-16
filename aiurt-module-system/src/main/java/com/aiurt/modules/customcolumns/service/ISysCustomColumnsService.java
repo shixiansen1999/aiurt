aab374cddde8c87dc67a26fb2b5e7fd308d0c5f7
huangqiyao <1667981208@qq.com>
2023-02-16 18:18:42 +0800

    feat(查询用户自定义列表):借口
    (cherry picked from commit abadf508bcd337aa9e4c1dea15dd26d516e64c33)

---
+++ aiurt-module-system/src/main/java/com/aiurt/modules/customcolumns/service/ISysCustomColumnsService.java
@@ -0,0 +1,15 @@
+package com.aiurt.modules.customcolumns.service;
+
+
+import com.aiurt.modules.customcolumns.entity.SysCustomColumns;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * @Description: sys_custom_columns
+ * @Author: aiurt
+ * @Date:   2023-02-16
+ * @Version: V1.0
+ */
+public interface ISysCustomColumnsService extends IService<SysCustomColumns> {
+
+}
