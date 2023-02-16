abc55b005c89189f1040667fb0ecceb8fb8b1c05
zhangwenlong <13797704736@163.com>
2023-02-16 18:39:07 +0800

    feat(系统配置):统计

---
+++ aiurt-module-system/src/main/java/com/aiurt/modules/usageconfig/dto/UsageConfigParamDTO.java
@@ -0,0 +1,23 @@
+package com.aiurt.modules.usageconfig.dto;
+
+import lombok.Data;
+
+/**
+ * @author zwl
+ */
+@Data
+public class UsageConfigParamDTO {
+
+
+    private String usageConfigParamId;
+
+    private String tableName;
+
+    private String staCondition;
+
+    private String state;
+
+    private String startTime;
+
+    private String endTime;
+}
