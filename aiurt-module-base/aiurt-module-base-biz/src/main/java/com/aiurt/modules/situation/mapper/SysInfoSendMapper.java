b4c705ec251df97c6d5465d1036ec07f423cd892
chendaoyue <14767753450@163.com>
2023-03-01 18:44:06 +0800

    fix(特情管理）新增对特情信息的编辑与删除功能

---
+++ aiurt-module-base/aiurt-module-base-biz/src/main/java/com/aiurt/modules/situation/mapper/SysInfoSendMapper.java
@@ -0,0 +1,17 @@
+package com.aiurt.modules.situation.mapper;
+
+
+import com.aiurt.modules.situation.entity.SysAnnouncementSend;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * @Description: 用户通告阅读标记表
+ * @Author: jeecg-boot
+ * @Date:  2019-02-21
+ * @Version: V1.0
+ */
+public interface SysInfoSendMapper extends BaseMapper<SysAnnouncementSend> {
+
+
+
+}
