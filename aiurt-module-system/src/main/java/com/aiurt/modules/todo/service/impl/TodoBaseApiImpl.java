3fdfc4c13421d31c6713680ffb11864b99542547
lkj <likangjie1221@163.com>
2023-03-07 10:34:07 +0800

    fix(消息发送bug修改):

--- aiurt-module-system/src/main/java/com/aiurt/modules/todo/service/impl/TodoBaseApiImpl.java
+++ aiurt-module-system/src/main/java/com/aiurt/modules/todo/service/impl/TodoBaseApiImpl.java
@@ -188,6 +188,12 @@ public class TodoBaseApiImpl implements ISTodoBaseAPI {
                 } else {
                     messageDTO.setBusType(sysTodoList.getProcessCode());
                 }
+                messageDTO.setBusKey(sysTodoList.getBusinessKey());
+                if (sysTodoList.getBusinessType() != null) {
+                    messageDTO.setBusType(sysTodoList.getBusinessType());
+                } else {
+                    messageDTO.setBusType(sysTodoList.getProcessCode());
+                }
                 qywxSendMsgHandle.sendMessage(messageDTO);
             }
         }
