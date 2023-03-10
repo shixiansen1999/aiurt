94b3787994250a31456eab06d0ae517d3d174cc0
sbx <26050364543@qq.com>
2023-03-10 11:27:55 +0800

    fix(巡视标准): 新增“标准表类型”字段（包括模板excel）

--- aiurt-module-patrol/aiurt-module-patrol-biz/src/main/java/com/aiurt/boot/standard/entity/PatrolStandard.java
+++ aiurt-module-patrol/aiurt-module-patrol-biz/src/main/java/com/aiurt/boot/standard/entity/PatrolStandard.java
@@ -44,6 +44,14 @@ public class PatrolStandard extends DictEntity implements Serializable {
 	@Excel(name = "巡视标准表名称", width = 15,needMerge = true)
     @ApiModelProperty(value = "巡视标准表名称")
     private java.lang.String name;
+    /**标准表类型*/
+    @ApiModelProperty(value = "巡视标准表类型:0应急/1车载/2正线/3车辆段")
+    private java.lang.Integer standardType;
+    /**标准表类型名称*/
+    @Excel(name = "标准表类型", width = 15, needMerge = true)
+    @ApiModelProperty(value = "巡视标准表类型:0应急/1车载/2正线/3车辆段")
+    @TableField(exist = false)
+    private java.lang.String standardTypeName;
 	/**专业code*/
     @Excel(name = "适用专业", width = 15,needMerge = true)
     @ApiModelProperty(value = "专业code")
