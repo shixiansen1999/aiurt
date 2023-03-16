75be27719b4f1e4451d1b078e6fae109c28396e4
chendaoyue <14767753450@163.com>
2023-03-16 16:12:22 +0800

    feat(规程规范与知识库）增加允许查看，下载，删除，编辑，可管理权限一键添加功能

--- aiurt-module-base/aiurt-module-base-api/src/main/java/com/aiurt/modules/sysfile/vo/SysFileTypeDetailVO.java
+++ aiurt-module-base/aiurt-module-base-api/src/main/java/com/aiurt/modules/sysfile/vo/SysFileTypeDetailVO.java
@@ -24,11 +24,17 @@ public class SysFileTypeDetailVO extends SysFileType implements Serializable {
 	@ApiModelProperty("原可查看")
 	private Set<SimpUserVO> primaryLookStatus;
 
+	@ApiModelProperty("原可查看筛选")
+	private List<String> primaryLookStatusCode;
+
 	@ApiModelProperty("可查看")
 	private Set<SimpUserVO> lookUsers;
 
 	@ApiModelProperty("原可编辑")
 	private Set<SimpUserVO> primaryEditStatus;
+
+	@ApiModelProperty("原可编辑筛选")
+	private List<String> primaryEditStatusCode;
 
 	@ApiModelProperty("可编辑")
 	private Set<SimpUserVO> editUsers;
@@ -36,17 +42,26 @@ public class SysFileTypeDetailVO extends SysFileType implements Serializable {
 	@ApiModelProperty("原可上传")
 	private Set<SimpUserVO> primaryUploadStatus;
 
+	@ApiModelProperty("管理员权限筛选")
+	private List<String> primaryUploadStatusCode;
+
 	@ApiModelProperty("可上传")
 	private Set<SimpUserVO> uploadStatus;
 
 	@ApiModelProperty("原可下载")
 	private Set<SimpUserVO> primaryDownloadStatus;
 
+	@ApiModelProperty("原可下载筛选")
+	private List<String> primaryDownloadStatusCode;
+
 	@ApiModelProperty("可下载")
 	private Set<SimpUserVO> downloadStatus;
 
 	@ApiModelProperty("原可删除")
 	private Set<SimpUserVO> primaryDeleteStatus;
+
+	@ApiModelProperty("原可删除筛选")
+	private List<String> primaryDeleteStatusCode;
 
 	@ApiModelProperty("可删除")
 	private Set<SimpUserVO> deleteStatus;
@@ -60,9 +75,11 @@ public class SysFileTypeDetailVO extends SysFileType implements Serializable {
 	@ApiModelProperty("原可在线编辑")
 	private Set<SimpUserVO> primaryOnlineEditing;
 
+	@ApiModelProperty("原可在线编辑筛选")
+	private List<String> primaryOnlineEditingCode;
+
 	@ApiModelProperty("可在线编辑")
 	private Set<SimpUserVO> onlineEditing;
 
-	@ApiModelProperty("可在线编辑筛选")
-	private List<String> editUsersCode;
+
 }
