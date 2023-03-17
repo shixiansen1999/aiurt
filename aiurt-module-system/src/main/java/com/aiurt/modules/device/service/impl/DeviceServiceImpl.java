044a3695d7e7e006649cf4789b8d6131b3403a72
lkj <likangjie1221@163.com>
2023-03-17 16:49:54 +0800

    fix(设备主数据导入bug修改):
    (cherry picked from commit 5172a89bfdb2d2ae20aa7a01930ec6435014b95e)

--- aiurt-module-system/src/main/java/com/aiurt/modules/device/service/impl/DeviceServiceImpl.java
+++ aiurt-module-system/src/main/java/com/aiurt/modules/device/service/impl/DeviceServiceImpl.java
@@ -660,6 +660,9 @@ public class DeviceServiceImpl extends ServiceImpl<DeviceMapper, Device> impleme
 		if (StrUtil.isNotEmpty(stationCodeName)) {
 			LambdaQueryWrapper<CsStation> csStationWrapper = new LambdaQueryWrapper<>();
 			csStationWrapper.eq(CsStation::getStationName, stationCodeName).eq(CsStation::getDelFlag, 0);
+			if (StrUtil.isNotEmpty(device.getLineCode())) {
+				csStationWrapper.eq(CsStation::getLineCode, device.getLineCode());
+			}
 			CsStation one = csStationService.getOne(csStationWrapper);
 			if (ObjectUtil.isEmpty(one)) {
 				stringBuilder.append("系统不存在该站点，");
