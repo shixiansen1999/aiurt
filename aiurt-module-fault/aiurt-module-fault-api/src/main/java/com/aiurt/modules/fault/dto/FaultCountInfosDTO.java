fa2c1b722f20da59731af71335b1c0447252d2a0
sbx <26050364543@qq.com>
2023-03-23 11:45:47 +0800

    fix(首页故障未解决列表): 故障现象分类未显示bug修复

--- aiurt-module-fault/aiurt-module-fault-api/src/main/java/com/aiurt/modules/fault/dto/FaultCountInfosDTO.java
+++ aiurt-module-fault/aiurt-module-fault-api/src/main/java/com/aiurt/modules/fault/dto/FaultCountInfosDTO.java
@@ -88,10 +88,11 @@ public class FaultCountInfosDTO {
     @TableField(exist = false)
     private String deviceName;
 
-    /**故障现象*/
-    @ApiModelProperty(value = "故障现象",  required = true)
+    /**故障现象分类*/
+    @ApiModelProperty(value = "故障现象分类",  required = true)
     @NotBlank(message = "请填写故障现象!")
     @Length(max = 255, message = "故障现象长度不能超过255")
+    @Dict(dictTable = "fault_knowledge_base_type", dicCode = "code", dicText = "name")
     private String faultPhenomenon;
 
     /**故障发生时间*/
