394c3784b658d2a908c4f56cb1a88766f5091260
lkj <likangjie1221@163.com>
2023-03-23 18:09:23 +0800

    fix(故障接口判空):

--- aiurt-module-fault/aiurt-module-fault-biz/src/main/java/com/aiurt/modules/fault/controller/FaultController.java
+++ aiurt-module-fault/aiurt-module-fault-biz/src/main/java/com/aiurt/modules/fault/controller/FaultController.java
@@ -152,7 +152,9 @@ public class FaultController extends BaseController<Fault, IFaultService> {
             queryWrapper.in("fault_phenomenon", faultPhenomenonCodes);
             queryWrapper.or().like("code", faultPhenomenon);
         } else {
-            queryWrapper.like("code", faultPhenomenon);
+            if (StrUtil.isNotBlank(faultPhenomenon)) {
+                queryWrapper.like("code", faultPhenomenon);
+            }
         }
         queryWrapper.apply(StrUtil.isNotBlank(stationCode), "(line_code = {0} or station_code = {0} or station_position_code = {0})", stationCode);
         queryWrapper.apply(StrUtil.isNotBlank(fault.getDevicesIds()), "(code in (select fault_code from fault_device where device_code like  concat('%', {0}, '%')))", fault.getDevicesIds());
