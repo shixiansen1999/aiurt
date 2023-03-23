ef0b29fa4158395f0e1978afddc709c65a9dc3c4
cjb <chenjiabao@aiurt.onaliyun.com>
2023-03-23 18:44:45 +0800

    fix(系统脚本和搜索引擎模块): 脚本增加ES环境变量替换，索引映射文件修改

--- aiurt-module-system/src/main/assembly/docker/bin/aiurt-platfrom-replace-env.sh
+++ aiurt-module-system/src/main/assembly/docker/bin/aiurt-platfrom-replace-env.sh
@@ -4,6 +4,7 @@ mysqlname=$DB_NAME
 mysqlport=$DB_PROT
 redisip=$REDIS_IP
 redisport=$REDIS_PORT
+esurl=$ES_URL
 if [ ! $mysqlip ]; then
     mysqlip="127.0.0.1"
 fi
@@ -19,20 +20,25 @@ fi
 if [ ! $redisport ]; then
     redisport="6379"
 fi
+if [ ! $esurl ]; then
+    esurl="http://127.0.0.1:9200"
+fi
 
 applicationYml="/aiurt-platfrom/aiurt-module-system/config/application-docker.yml"
-if [ ! -z "$mysqlip" ] && [ ! -z "$mysqlname" ] && [ ! -z "$mysqlport" ]  && [ ! -z "$redisip" ]  && [ ! -z "$redisport" ]; then
+if [ ! -z "$mysqlip" ] && [ ! -z "$mysqlname" ] && [ ! -z "$mysqlport" ]  && [ ! -z "$redisip" ]  && [ ! -z "$redisport" ] && [ ! -z "$esurl" ]; then
   needReMySqlIpChar="\$MYSQL_IP"
   needReMySqlNameChar="\$DB_NAME"
   needReMySqlPortChar="\$DB_PROT"
   needReRedisIpChar="\$REDIS_IP"
   needReRedisPortChar="\$REDIS_PORT"
+  needEsUrlChar="\$ES_URL"
   if [ -f "$applicationYml"  ]; then
     sed -i "s|$needReMySqlIpChar|$mysqlip|g" $applicationYml
     sed -i "s|$needReMySqlNameChar|$mysqlname|g" $applicationYml
     sed -i "s|$needReMySqlPortChar|$mysqlport|g" $applicationYml
     sed -i "s|$needReRedisIpChar|$redisip|g" $applicationYml
     sed -i "s|$needReRedisPortChar|$redisport|g" $applicationYml
+    sed -i "s|$needEsUrlChar|$esurl|g" $applicationYml
   fi
 fi
 cd /aiurt-platfrom/aiurt-module-system
