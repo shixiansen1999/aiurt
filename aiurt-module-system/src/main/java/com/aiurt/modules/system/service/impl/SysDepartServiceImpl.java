f6611e0a30424aea2ba4326604c034bff3c46293
hlq <1317009531@qq.com>
2023-04-02 12:23:00 +0800

    fix(备件类型-):备件消耗数量修复

--- aiurt-module-system/src/main/java/com/aiurt/modules/system/service/impl/SysDepartServiceImpl.java
+++ aiurt-module-system/src/main/java/com/aiurt/modules/system/service/impl/SysDepartServiceImpl.java
@@ -132,28 +132,28 @@ public class SysDepartServiceImpl extends ServiceImpl<SysDepartMapper, SysDepart
 		query.orderByAsc(SysDepart::getDepartOrder);
 		query.orderByDesc(SysDepart::getCreateTime);
 		List<SysDepart> list = this.list(query);
-		if (flag){
-			LoginUser sysUser = (LoginUser)SecurityUtils.getSubject().getPrincipal();
-			List<Map<String,String>> users=sysUserMapper.getRealNameMap(sysUser.getOrgId());
-			System.out.println(users);
-			Map<String, String> userMap = users.stream()
-					.collect(Collectors.toMap(a1->a1.get("userId"),a2->a2.get("realname"),(a1,a2)->a1));
-			list.forEach(l->{
-				String managerId=l.getManagerId();
-				if (StrUtil.isNotEmpty(l.getManagerId())&&userMap.containsKey(managerId)){
-					String realName= userMap.get(l.getManagerId());
-					l.setManagerName(realName);
-				}else {
-					l.setManagerId(null);
-				}
-				if (StrUtil.isNotEmpty(l.getTechnicalId())&&userMap.containsKey(l.getManagerId())){
-					String realName= userMap.get(l.getTechnicalId());
-					l.setTechnicalName(realName);
-				}else {
-					l.setTechnicalId(null);
-				}
-			});
-		}
+//		if (flag){
+//			LoginUser sysUser = (LoginUser)SecurityUtils.getSubject().getPrincipal();
+//			List<Map<String,String>> users=sysUserMapper.getRealNameMap(sysUser.getOrgId());
+//			System.out.println(users);
+//			Map<String, String> userMap = users.stream()
+//					.collect(Collectors.toMap(a1->a1.get("userId"),a2->a2.get("realname"),(a1,a2)->a1));
+//			list.forEach(l->{
+//				String managerId=l.getManagerId();
+//				if (StrUtil.isNotEmpty(l.getManagerId())&&userMap.containsKey(managerId)){
+//					String realName= userMap.get(l.getManagerId());
+//					l.setManagerName(realName);
+//				}else {
+//					l.setManagerId(null);
+//				}
+//				if (StrUtil.isNotEmpty(l.getTechnicalId())&&userMap.containsKey(l.getManagerId())){
+//					String realName= userMap.get(l.getTechnicalId());
+//					l.setTechnicalName(realName);
+//				}else {
+//					l.setTechnicalId(null);
+//				}
+//			});
+//		}
         //update-begin---author:wangshuai ---date:20220307  for：[JTC-119]在部门管理菜单下设置部门负责人 创建用户的时候不需要处理
 		//设置用户id,让前台显示
         this.setUserIdsByDepList(list);
