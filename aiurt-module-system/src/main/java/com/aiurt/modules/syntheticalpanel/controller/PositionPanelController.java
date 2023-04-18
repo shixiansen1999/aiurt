8fff324c2df65416cc4aed3072baebfc7e386d4f
zhangwenlong <13797704736@163.com>
2023-04-18 15:31:50 +0800

    feat: 根据站点信息查询视频监控设备
    (cherry picked from commit 2f3e0d08568fb15ce04ed7a067c66ecba93e0272)

--- aiurt-module-system/src/main/java/com/aiurt/modules/syntheticalpanel/controller/PositionPanelController.java
+++ aiurt-module-system/src/main/java/com/aiurt/modules/syntheticalpanel/controller/PositionPanelController.java
@@ -1,18 +1,30 @@
 package com.aiurt.modules.syntheticalpanel.controller;
 
+import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.util.StrUtil;
 import com.aiurt.common.aspect.annotation.AutoLog;
 import com.aiurt.common.aspect.annotation.PermissionData;
+import com.aiurt.common.constant.CommonConstant;
+import com.aiurt.modules.device.entity.Device;
+import com.aiurt.modules.device.entity.DeviceType;
+import com.aiurt.modules.device.service.IDeviceService;
+import com.aiurt.modules.device.service.IDeviceTypeService;
 import com.aiurt.modules.position.entity.CsStation;
 import com.aiurt.modules.syntheticalpanel.model.PositionPanelModel;
 import com.aiurt.modules.syntheticalpanel.service.PositionPanelService;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.jeecg.common.api.vo.Result;
+import org.jeecg.common.system.api.ISysBaseAPI;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @Description: 综合看板线路信息
@@ -27,6 +39,15 @@ public class PositionPanelController {
 
     @Autowired
     private PositionPanelService positionPanelService;
+
+    @Autowired
+    private IDeviceTypeService deviceTypeService;
+
+    @Autowired
+    private IDeviceService deviceService;
+
+    @Autowired
+    private ISysBaseAPI iSysBaseAPI;
 
 
     /**
@@ -70,5 +91,48 @@ public class PositionPanelController {
     public Result<String> edit(@RequestBody PositionPanelModel positionPanel) {
         positionPanelService.edit(positionPanel);
         return Result.OK("编辑成功!");
+    }
+
+    /**
+     * 通过站点查询
+     *
+     * @param stationCode
+     * @return
+     */
+    @AutoLog(value = "综合看板监控设备信息-通过站点查询", operateType =  1, operateTypeAlias = "查询-通过站点查询", permissionUrl = "")
+    @ApiOperation(value="综合看板监控设备信息-通过站点查询", notes="综合看板监控设备信息-通过站点查询")
+    @PostMapping(value = "/getMonitorDevice")
+    public Result<IPage<Device>> getMonitorDevice(@RequestParam(name="stationCode",required=true)  String stationCode,
+                                                  @RequestParam(name = "pageNo", defaultValue = "1") Integer pageNo,
+                                                  @RequestParam(name = "pageSize", defaultValue = "10") Integer pageSize
+                                                  ){
+        String string = "摄像机";
+        Result<IPage<Device>> result = new Result<>();
+        LambdaQueryWrapper<DeviceType> deviceTypeLambdaQueryWrapper = new LambdaQueryWrapper<>();
+        deviceTypeLambdaQueryWrapper.eq(DeviceType::getDelFlag,CommonConstant.DEL_FLAG_0)
+                  .eq(DeviceType::getName,string);
+        List<DeviceType> deviceTypeList = deviceTypeService.list(deviceTypeLambdaQueryWrapper);
+
+        Page<Device> devicePage = new Page<>();
+        if (CollUtil.isNotEmpty(deviceTypeList)){
+            List<String> collect = deviceTypeList.stream().map(DeviceType::getCode).collect(Collectors.toList());
+            LambdaQueryWrapper<Device> deviceLambdaQueryWrapper = new LambdaQueryWrapper<>();
+            deviceLambdaQueryWrapper.eq(Device::getDelFlag,CommonConstant.DEL_FLAG_0)
+                    .eq(Device::getStationCode,stationCode)
+                    .in(Device::getDeviceTypeCode,collect);
+           devicePage = deviceService.page(new Page<>(pageNo, pageSize), deviceLambdaQueryWrapper);
+        }
+        if (CollUtil.isNotEmpty(devicePage.getRecords())){
+            devicePage.getRecords().forEach(e->{
+                if(StrUtil.isNotBlank(e.getStationCode())){
+                    String position = iSysBaseAPI.getPosition(e.getStationCode());
+                    e.setStationCodeName(position);
+                }
+            });
+        }
+        result.setSuccess(true);
+        result.setResult(devicePage);
+        result.setCode(CommonConstant.SC_OK_200);
+        return result;
     }
 }
