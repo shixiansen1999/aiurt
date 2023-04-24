79d01448f15d357847c83683a11cbdaa70629b90
zhangwenlong <13797704736@163.com>
2023-04-24 18:57:56 +0800

    feat: 防汛信息模块实现
    (cherry picked from commit 3b72ee48f539f4ace788c85a5d04a1ba8b615efe)

---
+++ aiurt-module-system/src/main/java/com/aiurt/modules/floodpreventioninformation/service/impl/FloodPreventionInformationServiceImpl.java
@@ -0,0 +1,89 @@
+package com.aiurt.modules.floodpreventioninformation.service.impl;
+
+import cn.afterturn.easypoi.excel.ExcelExportUtil;
+import cn.afterturn.easypoi.excel.entity.TemplateExportParams;
+import com.aiurt.modules.floodpreventioninformation.entity.FloodPreventionInformation;
+import com.aiurt.modules.floodpreventioninformation.mapper.FloodPreventionInformationMapper;
+import com.aiurt.modules.floodpreventioninformation.service.IFloodPreventionInformationService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.apache.commons.io.FileUtils;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.jeecg.common.api.vo.Result;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @Description: flood_prevention_information
+ * @Author: zwl
+ * @Date:   2023-04-24
+ * @Version: V1.0
+ */
+@Service
+public class FloodPreventionInformationServiceImpl extends ServiceImpl<FloodPreventionInformationMapper, FloodPreventionInformation> implements IFloodPreventionInformationService {
+
+    @Override
+    public Result<?> importExcel(HttpServletRequest request, HttpServletResponse response) {
+        MultipartHttpServletRequest multipartRequest = (MultipartHttpServletRequest) request;
+        Map<String, MultipartFile> fileMap = multipartRequest.getFileMap();
+
+        List<String> errorMessage = new ArrayList<>();
+
+        int successLines = 0;
+        String url = null;
+        // 错误信息
+        int  errorLines = 0;
+        for (Map.Entry<String, MultipartFile> entity : fileMap.entrySet()) {
+
+        }
+        return null;
+    }
+
+    @Override
+    public void exportTemplateXl(HttpServletResponse response) throws IOException {
+        //获取输入流，原始模板位置
+        Resource resource = new ClassPathResource("/templates/floodInformation.xlsx");
+        InputStream resourceAsStream = resource.getInputStream();
+
+        //2.获取临时文件
+        File fileTemp= new File("/templates/floodInformation.xlsx");
+        try {
+            //将读取到的类容存储到临时文件中，后面就可以用这个临时文件访问了
+            FileUtils.copyInputStreamToFile(resourceAsStream, fileTemp);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+        }
+
+
+        String path = fileTemp.getAbsolutePath();
+        TemplateExportParams exportParams = new TemplateExportParams(path);
+        Map<Integer, Map<String, Object>> sheetsMap = new HashMap<>();
+        Workbook workbook =  ExcelExportUtil.exportExcel(sheetsMap, exportParams);
+        String fileName = "防汛信息导入模板.xlsx";
+
+        try {
+            response.setHeader("Content-Disposition",
+                    "attachment;filename=" + new String(fileName.getBytes("UTF-8"), "iso8859-1"));
+            response.setHeader("Content-Disposition", "attachment;filename="+"防汛信息导入模板.xlsx");
+            BufferedOutputStream bufferedOutPut = new BufferedOutputStream(response.getOutputStream());
+            workbook.write(bufferedOutPut);
+            bufferedOutPut.flush();
+            bufferedOutPut.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
