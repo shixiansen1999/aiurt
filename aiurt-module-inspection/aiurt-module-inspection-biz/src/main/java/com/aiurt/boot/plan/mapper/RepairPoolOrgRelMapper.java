f6fce672c1ae2c56ccf8988e39dcbcd6882edb48
wgp <123456@qq.com>
2023-05-04 18:48:59 +0800

    fix(性能优化):性能优化

--- aiurt-module-inspection/aiurt-module-inspection-biz/src/main/java/com/aiurt/boot/plan/mapper/RepairPoolOrgRelMapper.java
+++ aiurt-module-inspection/aiurt-module-inspection-biz/src/main/java/com/aiurt/boot/plan/mapper/RepairPoolOrgRelMapper.java
@@ -1,8 +1,12 @@
 package com.aiurt.boot.plan.mapper;
 
+import com.aiurt.boot.index.dto.MapDTO;
 import com.aiurt.boot.plan.entity.RepairPoolOrgRel;
 import com.aiurt.common.aspect.annotation.EnableDataPerm;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+import java.util.Collection;
+import java.util.List;
 
 /**
  * @Description: repair_pool_org_rel
@@ -12,5 +16,11 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
  */
 @EnableDataPerm
 public interface RepairPoolOrgRelMapper extends BaseMapper<RepairPoolOrgRel> {
-
+    /**
+     * 根据code查询检修任务对应的组织机构编码
+     *
+     * @param planCodes
+     * @return
+     */
+    List<MapDTO> selectOrgByCode(List<String> planCodes);
 }
