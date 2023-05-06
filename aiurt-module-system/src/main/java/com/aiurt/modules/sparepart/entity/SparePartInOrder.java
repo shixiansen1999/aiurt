c90da5709dd594a469dfd7c1f4a75d0c8bd16070
hlq <1317009531@qq.com>
2023-05-06 18:33:03 +0800

    fix(备件入库)需求说写死，根据管理部门权限过滤备件申领列表数据

--- aiurt-module-system/src/main/java/com/aiurt/modules/sparepart/entity/SparePartInOrder.java
+++ aiurt-module-system/src/main/java/com/aiurt/modules/sparepart/entity/SparePartInOrder.java
@@ -1,25 +1,23 @@
 package com.aiurt.modules.sparepart.entity;
 
-import java.io.Serializable;
-import java.io.UnsupportedEncodingException;
-import java.util.Date;
-import java.math.BigDecimal;
-import java.util.List;
-
 import com.aiurt.common.aspect.annotation.DeptFilterColumn;
+import com.aiurt.common.aspect.annotation.Dict;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
-import lombok.Data;
 import com.fasterxml.jackson.annotation.JsonFormat;
-import org.springframework.format.annotation.DateTimeFormat;
-import org.jeecgframework.poi.excel.annotation.Excel;
-import com.aiurt.common.aspect.annotation.Dict;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
+import org.jeecgframework.poi.excel.annotation.Excel;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
 
 /**
  * @Description: spare_part_in_order
@@ -179,4 +177,7 @@ public class SparePartInOrder implements Serializable {
 	@ApiModelProperty(value = "所属部门")
 	@DeptFilterColumn
 	private String sysOrgCode;
+	@ApiModelProperty("管理部门")
+	@TableField(exist = false)
+	private List<String> orgCodes;
 }
