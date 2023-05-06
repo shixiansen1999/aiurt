7305dfe7f36f51667755f223a889a57de5ea5d59
fgw <fugw@aiurt.onaliyun.com>
2023-05-06 14:57:37 +0800

    feat(脚本修改) 获取天气

--- aiurt-module-system/src/main/java/com/aiurt/modules/basic/service/impl/SystemIndexServiceImpl.java
+++ aiurt-module-system/src/main/java/com/aiurt/modules/basic/service/impl/SystemIndexServiceImpl.java
@@ -1,7 +1,10 @@
 package com.aiurt.modules.basic.service.impl;
 
+import cn.hutool.core.util.StrUtil;
 import com.aiurt.modules.basic.dto.WeatherDetailDTO;
 import com.aiurt.modules.basic.service.ISystemIndexService;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -33,6 +36,24 @@ public class SystemIndexServiceImpl implements ISystemIndexService, Serializable
     public WeatherDetailDTO getWeatherInfo() {
 
 
-        return new WeatherDetailDTO();
+        String resultJson = restTemplate.getForObject(String.format("%s?key=%s&city=%s&extensions=base", url, key, city), String.class);
+
+        log.info("请求获取的天气结果:{}", resultJson);
+
+        if (StrUtil.isBlank(resultJson)) {
+            return new WeatherDetailDTO();
+        }
+
+        JSONObject jsonObject = JSONObject.parseObject(resultJson);
+
+        String status = jsonObject.getString("status");
+        if (StrUtil.equalsIgnoreCase("0", status)) {
+            log.info("请求数据失败");
+            return new WeatherDetailDTO();
+        }
+
+        JSONArray lives = jsonObject.getJSONArray("lives");
+        WeatherDetailDTO detailDTO = lives.getObject(0, WeatherDetailDTO.class);
+        return detailDTO;
     }
 }
