c0b4fbfc269b0333f9646e7e485a2c6ac5c2c5d9
cjb <chenjiabao@aiurt.onaliyun.com>
2023-05-26 14:59:18 +0800

    feat(故障知识库): 故障原因使用记录表实体类，service层等基本代码

---
+++ aiurt-module-fault/aiurt-module-fault-biz/src/main/java/com/aiurt/modules/faultcauseusagerecords/service/impl/FaultCauseUsageRecordsServiceImpl.java
@@ -0,0 +1,17 @@
+package com.aiurt.modules.faultcauseusagerecords.service.impl;
+
+import com.aiurt.modules.faultcauseusagerecords.entity.FaultCauseUsageRecords;
+import com.aiurt.modules.faultcauseusagerecords.mapper.FaultCauseUsageRecordsMapper;
+import com.aiurt.modules.faultcauseusagerecords.service.IFaultCauseUsageRecordsService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * @Description: 故障原因使用记录表
+ * @Author: aiurt
+ * @Version: V1.0
+ */
+@Service
+public class FaultCauseUsageRecordsServiceImpl extends ServiceImpl<FaultCauseUsageRecordsMapper, FaultCauseUsageRecords> implements IFaultCauseUsageRecordsService {
+
+}
