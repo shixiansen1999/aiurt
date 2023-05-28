d38b8d7e61bed80f523d09167cf55175bf8745fe
huayiwei <2408340418@qq.com>
2023-05-28 15:50:39 +0800

    feat(wifi连接记录)：修改wifi连接问题

--- aiurt-module-system/src/main/java/com/aiurt/modules/system/service/impl/SysUserPositionCurrentServiceImpl.java
+++ aiurt-module-system/src/main/java/com/aiurt/modules/system/service/impl/SysUserPositionCurrentServiceImpl.java
@@ -1,7 +1,10 @@
 package com.aiurt.modules.system.service.impl;
 
+import cn.hutool.core.date.DateUnit;
+import cn.hutool.core.date.DateUtil;
 import cn.hutool.core.util.ObjectUtil;
 import cn.hutool.core.util.StrUtil;
+import com.aiurt.boot.constant.SysParamCodeConstant;
 import com.aiurt.common.exception.AiurtBootException;
 import com.aiurt.modules.system.entity.SysUserPositionCurrent;
 import com.aiurt.modules.system.mapper.SysUserPositionCurrentMapper;
@@ -9,7 +12,12 @@ import com.aiurt.modules.system.service.ISysUserPositionCurrentService;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import lombok.extern.slf4j.Slf4j;
+import org.jeecg.common.system.api.ISysParamAPI;
+import org.jeecg.common.system.vo.SysParamModel;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
 
 /**
  * 用户连接站点wifi的当前位置
@@ -18,6 +26,9 @@ import org.springframework.stereotype.Service;
 @Service
 @Slf4j
 public class SysUserPositionCurrentServiceImpl extends ServiceImpl<SysUserPositionCurrentMapper, SysUserPositionCurrent> implements ISysUserPositionCurrentService{
+
+    @Autowired
+    private ISysParamAPI sysParamApi;
 
     @Override
     public void saveOrUpdateOne(SysUserPositionCurrent sysUserPositionCurrent) {
@@ -33,19 +44,33 @@ public class SysUserPositionCurrentServiceImpl extends ServiceImpl<SysUserPositi
         if (sysUserPositionCurrent.getStationCode() == null) {
             // 站点为空时，设置为异常
             sysUserPositionCurrent.setIsPositionError("1");
+        } else {
+            // 不为空，正常
+            sysUserPositionCurrent.setIsPositionError("0");
         }
+
         if (ObjectUtil.isNull(one)) {
             // 不存在，添加
             this.save(sysUserPositionCurrent);
-        }else {
-            // 已存在，更新
-            if (StrUtil.equals(one.getStationCode(), sysUserPositionCurrent.getStationCode())) {
-                // 同站点，或者都是null，不更新upload_time
-                sysUserPositionCurrent.setUploadTime(null);
-            }
-            sysUserPositionCurrent.setId(one.getId());
-            this.updateById(sysUserPositionCurrent);
+            return;
         }
+
+        // 已存在，更新
+        // 不更新create_time
+        sysUserPositionCurrent.setCreateTime(null);
+
+        SysParamModel filterParamModel = sysParamApi.selectByCode(SysParamCodeConstant.WIFI_UPDATE_INTERVAL);
+        String wifiUpdateInterval = filterParamModel.getValue();
+
+        // 如果one.getUpdateTime与new Date的时间间隔大于wifiUpdateInterval，就更新upload_time，不然就要进行判断是否同站点
+        int interval = (int) DateUtil.between(new Date(),
+                one.getUpdateTime() == null ? one.getCreateTime(): one.getUpdateTime(),  DateUnit.MINUTE);
+        if (interval <= Integer.parseInt(wifiUpdateInterval) && StrUtil.equals(one.getStationCode(), sysUserPositionCurrent.getStationCode())){
+            sysUserPositionCurrent.setUploadTime(null);
+        }
+
+        sysUserPositionCurrent.setId(one.getId());
+        this.updateById(sysUserPositionCurrent);
 
     }
 }
