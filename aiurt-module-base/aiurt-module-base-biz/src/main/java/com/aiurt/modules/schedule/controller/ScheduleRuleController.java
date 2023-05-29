fbd3ca44a9160a6c622daa4ffe860d9f7a4d6368
lkj <likangjie1221@163.com>
2023-05-29 17:43:33 +0800

    feat(班次变为基础数据需求，导出模板需要下拉框):

--- aiurt-module-base/aiurt-module-base-biz/src/main/java/com/aiurt/modules/schedule/controller/ScheduleRuleController.java
+++ aiurt-module-base/aiurt-module-base-biz/src/main/java/com/aiurt/modules/schedule/controller/ScheduleRuleController.java
@@ -1,13 +1,13 @@
 package com.aiurt.modules.schedule.controller;
 import cn.hutool.core.date.DateUtil;
 import cn.hutool.core.util.StrUtil;
+import com.aiurt.common.aspect.annotation.AutoLog;
 import com.aiurt.common.constant.CommonConstant;
-import com.aiurt.modules.schedule.service.IScheduleRuleItemService;
-import com.aiurt.modules.schedule.service.IScheduleRuleService;
+import com.aiurt.common.util.oConvertUtils;
 import com.aiurt.modules.schedule.entity.ScheduleRule;
 import com.aiurt.modules.schedule.entity.ScheduleRuleItem;
-import com.aiurt.common.aspect.annotation.AutoLog;
-import com.aiurt.common.util.oConvertUtils;
+import com.aiurt.modules.schedule.service.IScheduleRuleItemService;
+import com.aiurt.modules.schedule.service.IScheduleRuleService;
 import com.alibaba.fastjson.JSON;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
@@ -16,10 +16,8 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.StringUtils;
-import org.apache.shiro.SecurityUtils;
 import org.jeecg.common.api.vo.Result;
 import org.jeecg.common.system.query.QueryGenerator;
-import org.jeecg.common.system.vo.LoginUser;
 import org.jeecgframework.poi.excel.ExcelImportUtil;
 import org.jeecgframework.poi.excel.def.NormalExcelConstants;
 import org.jeecgframework.poi.excel.entity.ExportParams;
@@ -73,10 +71,6 @@ public class ScheduleRuleController {
                                                      @RequestParam(name = "pageNo", defaultValue = "1") Integer pageNo,
                                                      @RequestParam(name = "pageSize", defaultValue = "10") Integer pageSize,
                                                      HttpServletRequest req) {
-        //获取用户
-        LoginUser user = (LoginUser) SecurityUtils.getSubject().getPrincipal();
-        //根据登录用户的账号的查询条件筛选数据
-        scheduleRule.setCreateBy(user.getUsername());
         Result<IPage<ScheduleRule>> result = new Result<IPage<ScheduleRule>>();
         QueryWrapper<ScheduleRule> queryWrapper = QueryGenerator.initQueryWrapper(scheduleRule, req.getParameterMap());
         Page<ScheduleRule> page = new Page<ScheduleRule>(pageNo, pageSize);
