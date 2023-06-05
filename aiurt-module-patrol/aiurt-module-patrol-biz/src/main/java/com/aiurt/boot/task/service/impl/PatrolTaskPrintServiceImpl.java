78a1a957bd8419b571ba19aa826c563d6f014bf7
Michael <1785580740@qq.com>
2023-06-05 19:37:28 +0800

    feat(巡视打印)：封装方法

--- aiurt-module-patrol/aiurt-module-patrol-biz/src/main/java/com/aiurt/boot/task/service/impl/PatrolTaskPrintServiceImpl.java
+++ aiurt-module-patrol/aiurt-module-patrol-biz/src/main/java/com/aiurt/boot/task/service/impl/PatrolTaskPrintServiceImpl.java
@@ -4,6 +4,7 @@ package com.aiurt.boot.task.service.impl;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -260,10 +261,8 @@ public class PatrolTaskPrintServiceImpl extends PatrolTaskServiceImpl {
         String templateFileName = "patrol" +"/" + "template" + "/" + excelName;
         log.info("templateFileName:"+templateFileName);
         InputStream minioFile = MinioUtil.getMinioFile("platform",templateFileName);
-        Workbook workbookTpl = null;
-        CellRangeAddress mergeRegion = null;
-        Integer firstColumn = null;
-        Integer lastColumn = null;
+
+
 //        try {
 ////            inputStreamTemplate = new FileInputStream(templateFileName);
 //            workbookTpl = WorkbookFactory.create(minioFile);
@@ -278,122 +277,69 @@ public class PatrolTaskPrintServiceImpl extends PatrolTaskServiceImpl {
 //            throw new RuntimeException(e);
 //        }
 
-
-
-
-
-        // 全部放到内存里面 并填充
-        String fileName = patrolTask.getName() + System.currentTimeMillis() + ".xlsx";
-        String relatiePath = "/" + "patrol" + "/" + "print" + "/" + fileName;
-        String filePath = path +"/" +  fileName;
-        // 这里 会填充到第一个sheet， 然后文件流会自动关闭
-        // 查询头部数据
-        PrintPatrolTaskDTO taskDTO = new PrintPatrolTaskDTO();
-
-        Assert.notNull(patrolTask, "未找到对应记录！");
-        taskDTO.setId(patrolTask.getId());
-        taskDTO.setTitle(patrolTask.getName());
-        // 站点信息
-        List<PatrolTaskStationDTO> stationInfo = patrolTaskStationMapper.selectStationByTaskCode(patrolTask.getCode());
-        taskDTO.setStationNames(stationInfo.stream().map(PatrolTaskStationDTO::getStationName).collect(Collectors.joining()));
-        if (StrUtil.isNotEmpty(patrolTask.getEndUserId())) {
-            taskDTO.setUserName(patrolTaskMapper.getUsername(patrolTask.getEndUserId()));
-        }
-        if (StrUtil.isNotEmpty(patrolTask.getSpotCheckUserId())) {
-            taskDTO.setSpotCheckUserName(patrolTaskMapper.getUsername(patrolTask.getSpotCheckUserId()));
-        }
-        taskDTO.setSignUrl(patrolTask.getSignUrl());
-        Map<String, Object> map = MapUtils.newHashMap();
-        map.put("title",patrolTask.getName());
-        map.put("patrolStation", taskDTO.getStationNames());
-        map.put("patrolPerson", taskDTO.getUserName());
-        map.put("checkUserName",taskDTO.getSpotCheckUserName());
-        map.put("patrolDate", DateUtil.format(patrolTask.getSubmitTime(),"yyyy-MM-dd"));
-        map.put("patrolTime", DateUtil.format(patrolTask.getSubmitTime(),"HH:mm"));
-        Map<String, Object> imageMap = MapUtils.newHashMap();
-        if(StrUtil.isNotEmpty(taskDTO.getSignUrl())){
-            int index =  taskDTO.getSignUrl().indexOf("?");
-            SysAttachment sysAttachment = sysBaseApi.getFilePath(taskDTO.getSignUrl().substring(0, index));
-            InputStream inputStream = MinioUtil.getMinioFile("platform",sysAttachment.getFilePath());
-            if(ObjectUtil.isEmpty(inputStream)){
-                imageMap.put("signImage",null);
-            } else {
-                try {
-                    byte[] convert = FilePrintUtils.convert(inputStream);
-                    WriteCellData writeImageData = FilePrintUtils.writeCellImageData(convert);
-                    imageMap.put("signImage",writeImageData);
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-        }else{
-            imageMap.put("signImage",null);
-        }
-
-//        String imagePath = "C:\\Users\\14719\\Desktop\\1685182072119.jpg";
-//        // 设置图片数据
-//        File[] files = {new File(imagePath)};
-
+        //获取头部数据
+        PrintPatrolTaskDTO taskDTO = getHeaderData(patrolTask);
+        //填充头部Map
+        Map<String, Object> headerMap = getHeaderMap(patrolTask, taskDTO);
+        //文件打印签名
+        Map<String, Object> imageMap = getSignImageMap(taskDTO);
 
         //查询巡视标准详情
-        List<PrintDTO> patrolData = getWirelessSystem(id,map);
-        InputStream minioFile2 = MinioUtil.getMinioFile("platform",templateFileName);
-        try (ExcelWriter excelWriter = EasyExcel.write(filePath).withTemplate(minioFile2).build()) {
-            int[] mergeColumnIndex = {0,1,2};
-            CustomCellMergeHandler customCellMergeStrategy = new CustomCellMergeHandler(3,mergeColumnIndex);
-            WriteSheet writeSheet = EasyExcel.writerSheet().registerWriteHandler(customCellMergeStrategy).build();
-            FillConfig fillConfig = FillConfig.builder().forceNewRow(Boolean.FALSE).build();
-            //填充列表数据
-            excelWriter.fill(new FillWrapper("list",patrolData),fillConfig, writeSheet);
-            //填充表头
-            excelWriter.fill(map, writeSheet);
-            //填充图片
-            excelWriter.fill(imageMap, writeSheet);
-            excelWriter.finish();
-            int startRow = 3;
-            int endRow = startRow;
-            if (CollUtil.isNotEmpty(patrolData)){
-                endRow = startRow+patrolData.size()-1;
-            }
+        List<PrintDTO> patrolData = getPrintOthers(id,headerMap);
 
-//            try (InputStream inputStream = new FileInputStream(filePath);
-//                Workbook workbook = WorkbookFactory.create(inputStream)) {
-//                Sheet sheet = workbook.getSheetAt(0);
-////                sheet.setMargin(Sheet.TopMargin, 0.5); // 上边距
-////                sheet.setMargin(Sheet.BottomMargin, 0.5); // 下边距
-////                sheet.setMargin(Sheet.LeftMargin, 1); // 左边距
-////                sheet.setMargin(Sheet.RightMargin, 1); // 右边距
-//                FilePrintUtils.printSet(sheet);
-//                // 设置边距（单位为英寸）
-//                // 设置打印边距
-//                //自动换行
-//               // setWrapText(workbook,1,startRow,endRow,0,0);
-//                FilePrintUtils.addReturn(workbook,startRow,endRow,0,0);
-//                FilePrintUtils.setWrapText(workbook,7,1,1,1,1,true);
-//                FilePrintUtils.setWrapText(workbook,7,startRow,endRow,1,firstColumn>3?3:2,false);
-//                //合并指定范围行的单元格
-//                FilePrintUtils.mergeCellsInColumnRange(workbook,40,startRow,endRow,firstColumn,lastColumn);
-//
-//                //设置第一列列宽
-//                FilePrintUtils.setColumnWidth(sheet,0,10);
-//                // 保存修改后的Excel文件
-//                try (OutputStream outputStream = new FileOutputStream(filePath)) {
-//                    workbook.write(outputStream);
-//                }
-//            }
-            MinioUtil.upload(new FileInputStream(filePath),relatiePath);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-        SysAttachment sysAttachment = new SysAttachment();
-        sysAttachment.setFileName(fileName);
-        sysAttachment.setFilePath(relatiePath);
-        sysAttachment.setType("minio");
-        sysBaseApi.saveSysAttachment(sysAttachment);
+        SysAttachment sysAttachment = fillTemplateData(patrolTask,patrolData, templateFileName);
         return sysAttachment.getId()+"?fileName="+sysAttachment.getFileName();
     }
 
 
+    /**
+     * 电话系统模板
+     * @param patrolTask
+     * @param excelName
+     * @param patrolData
+     * @return
+     * @throws IOException
+     */
+    public String printPatrolTaskTelephoneSystem(PatrolTask patrolTask,String excelName,List<PrintDTO> patrolData) throws IOException {
+        // 模板注意 用{} 来表示你要用的变量 如果本来就有"{","}" 特殊字符 用"\{","\}"代替
+        // 填充list 的时候还要注意 模板中{.} 多了个点 表示list
+        // 如果填充list的对象是map,必须包涵所有list的key,哪怕数据为null，必须使用map.put(key,null)
+        String templateFileName = "patrol" +"/" + "template" + "/" + excelName;
+        log.info("templateFileName:"+templateFileName);
+        InputStream minioFile = MinioUtil.getMinioFile("platform",templateFileName);
+        Workbook workbookTpl = null;
+        CellRangeAddress mergeRegion = null;
+        Integer firstColumn = null;
+        Integer lastColumn = null;
+        try {
+            workbookTpl = WorkbookFactory.create(minioFile);
+            Sheet sheet = workbookTpl.getSheetAt(0);
+            mergeRegion = FilePrintUtils.findMergeRegions(sheet, "巡检标准");
+            firstColumn = mergeRegion.getFirstColumn();
+            lastColumn = mergeRegion.getLastColumn();
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+        String fileName = patrolTask.getName() + System.currentTimeMillis() + ".xlsx";
+        String relatiePath = "/" + "patrol" + "/" + "print" + "/" + fileName;
+        String filePath = path +"/" +  fileName;
+
+        SysAttachment sysAttachment = fillTemplateData(patrolTask,patrolData, templateFileName);
+        reProcessFile(patrolData,firstColumn, lastColumn, filePath);
+
+        return sysAttachment.getId()+"?fileName="+sysAttachment.getFileName();
+    }
+
+    /**
+     * 网管模板
+     * @param id
+     * @return
+     */
     public String printPatrolTaskByNetworkManage(String id){
         PatrolTask patrolTask = patrolTaskMapper.selectById(id);
         List<PatrolTaskStandard> patrolTaskStandard = patrolTaskStandardMapper.selectList(new LambdaQueryWrapper<PatrolTaskStandard>()
@@ -422,21 +368,7 @@ public class PatrolTaskPrintServiceImpl extends PatrolTaskServiceImpl {
         String filePath = path +"/" +  fileName;
         // 这里 会填充到第一个sheet， 然后文件流会自动关闭
         // 查询头部数据
-        PrintPatrolTaskDTO taskDTO = new PrintPatrolTaskDTO();
-
-        Assert.notNull(patrolTask, "未找到对应记录！");
-        taskDTO.setId(patrolTask.getId());
-        taskDTO.setTitle(patrolTask.getName());
-        // 站点信息
-        List<PatrolTaskStationDTO> stationInfo = patrolTaskStationMapper.selectStationByTaskCode(patrolTask.getCode());
-        taskDTO.setStationNames(stationInfo.stream().map(PatrolTaskStationDTO::getStationName).collect(Collectors.joining()));
-        if (StrUtil.isNotEmpty(patrolTask.getEndUserId())) {
-            taskDTO.setUserName(patrolTaskMapper.getUsername(patrolTask.getEndUserId()));
-        }
-        if (StrUtil.isNotEmpty(patrolTask.getSpotCheckUserId())) {
-            taskDTO.setSpotCheckUserName(patrolTaskMapper.getUsername(patrolTask.getSpotCheckUserId()));
-        }
-        taskDTO.setSignUrl(patrolTask.getSignUrl());
+        PrintPatrolTaskDTO taskDTO = getHeaderData(patrolTask);
         Map<String, Object> map = MapUtils.newHashMap();
         map.put("title",patrolTask.getName());
         map.put("patrolStation", taskDTO.getStationNames());
@@ -459,9 +391,8 @@ public class PatrolTaskPrintServiceImpl extends PatrolTaskServiceImpl {
         fillDataMap.put("性能监视-查询历史15分钟","☑异常");
 
 
-
         //查询巡视标准详情
-        List<PrintDTO> patrolData = getPrint(id,map);
+        List<PrintDTO> patrolData = getPrintOthers(id,map);
         InputStream minioFile2 = MinioUtil.getMinioFile("platform",templateFileName);
         try (ExcelWriter excelWriter = EasyExcel.write(filePath).withTemplate(minioFile2).build()) {
             int[] mergeColumnIndex = {0,1,2};
@@ -506,6 +437,136 @@ public class PatrolTaskPrintServiceImpl extends PatrolTaskServiceImpl {
         return sysAttachment.getId()+"?fileName="+sysAttachment.getFileName();
     }
 
+
+
+
+    /**
+     * 填充模板的数据
+     * @param patrolTask
+     * @param templateFileName
+     * @return
+     */
+    @NotNull
+    private SysAttachment fillTemplateData(PatrolTask patrolTask,List<PrintDTO> patrolData, String templateFileName) {
+        // 全部放到内存里面 并填充
+        String fileName = patrolTask.getName() + System.currentTimeMillis() + ".xlsx";
+        String relatiePath = "/" + "patrol" + "/" + "print" + "/" + fileName;
+        String filePath = path +"/" +  fileName;
+        //获取头部数据
+        PrintPatrolTaskDTO taskDTO = getHeaderData(patrolTask);
+        //填充头部Map
+        Map<String, Object> map = getHeaderMap(patrolTask, taskDTO);
+        //文件打印签名
+        Map<String, Object> imageMap = getSignImageMap(taskDTO);
+
+        //查询巡视标准详情
+//        List<PrintDTO> patrolData = getPrint(id,map);
+        InputStream minioFile2 = MinioUtil.getMinioFile("platform", templateFileName);
+        try (ExcelWriter excelWriter = EasyExcel.write(filePath).withTemplate(minioFile2).build()) {
+            int[] mergeColumnIndex = {0,1,2};
+            CustomCellMergeHandler customCellMergeStrategy = new CustomCellMergeHandler(3,mergeColumnIndex);
+            WriteSheet writeSheet = EasyExcel.writerSheet().registerWriteHandler(customCellMergeStrategy).build();
+            FillConfig fillConfig = FillConfig.builder().forceNewRow(Boolean.FALSE).build();
+            //填充列表数据
+            excelWriter.fill(new FillWrapper("list",patrolData),fillConfig, writeSheet);
+            //填充表头
+            excelWriter.fill(map, writeSheet);
+            //填充图片
+            excelWriter.fill(imageMap, writeSheet);
+            excelWriter.finish();
+
+            MinioUtil.upload(new FileInputStream(filePath),relatiePath);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        SysAttachment sysAttachment = new SysAttachment();
+        sysAttachment.setFileName(fileName);
+        sysAttachment.setFilePath(relatiePath);
+        sysAttachment.setType("minio");
+        sysBaseApi.saveSysAttachment(sysAttachment);
+        return sysAttachment;
+    }
+
+    /**
+     * 获取头部数据
+     * @param patrolTask
+     * @return
+     */
+    @NotNull
+    private PrintPatrolTaskDTO getHeaderData(PatrolTask patrolTask) {
+        // 查询头部数据
+        PrintPatrolTaskDTO taskDTO = new PrintPatrolTaskDTO();
+
+        Assert.notNull(patrolTask, "未找到对应记录！");
+        taskDTO.setId(patrolTask.getId());
+        taskDTO.setTitle(patrolTask.getName());
+        // 站点信息
+        List<PatrolTaskStationDTO> stationInfo = patrolTaskStationMapper.selectStationByTaskCode(patrolTask.getCode());
+        taskDTO.setStationNames(stationInfo.stream().map(PatrolTaskStationDTO::getStationName).collect(Collectors.joining()));
+        if (StrUtil.isNotEmpty(patrolTask.getEndUserId())) {
+            taskDTO.setUserName(patrolTaskMapper.getUsername(patrolTask.getEndUserId()));
+        }
+        if (StrUtil.isNotEmpty(patrolTask.getSpotCheckUserId())) {
+            taskDTO.setSpotCheckUserName(patrolTaskMapper.getUsername(patrolTask.getSpotCheckUserId()));
+        }
+        taskDTO.setSignUrl(patrolTask.getSignUrl());
+        return taskDTO;
+    }
+
+    /**
+     * 填充头部Map
+     * @param patrolTask
+     * @param taskDTO
+     * @return
+     */
+    @NotNull
+    private static Map<String, Object> getHeaderMap(PatrolTask patrolTask, PrintPatrolTaskDTO taskDTO) {
+        Map<String, Object> map = MapUtils.newHashMap();
+        map.put("title", patrolTask.getName());
+        map.put("patrolStation", taskDTO.getStationNames());
+        map.put("patrolPerson", taskDTO.getUserName());
+        map.put("checkUserName", taskDTO.getSpotCheckUserName());
+        map.put("patrolDate", DateUtil.format(patrolTask.getSubmitTime(),"yyyy-MM-dd"));
+        map.put("patrolTime", DateUtil.format(patrolTask.getSubmitTime(),"HH:mm"));
+        return map;
+    }
+
+    /**
+     * 再处理模板数据
+     * @param patrolData
+     * @param firstColumn
+     * @param lastColumn
+     * @param filePath
+     * @throws IOException
+     */
+    private static void reProcessFile(List<PrintDTO> patrolData,Integer firstColumn, Integer lastColumn, String filePath) throws IOException {
+        int startRow = 3;
+        int endRow = startRow;
+        if (!ObjectUtil.isEmpty(patrolData)){
+            endRow = startRow+patrolData.size()-1;
+        }
+        try (InputStream inputStream = new FileInputStream(filePath);
+             Workbook workbook = WorkbookFactory.create(inputStream)) {
+            Sheet sheet = workbook.getSheetAt(0);
+            FilePrintUtils.printSet(sheet);
+            //自动换行
+           // setWrapText(workbook,1,startRow,endRow,0,0);
+            FilePrintUtils.addReturn(workbook, startRow, endRow,0,0);
+            FilePrintUtils.setWrapText(workbook,7,1,1,1,1,true);
+            FilePrintUtils.setWrapText(workbook,7, startRow, endRow,1, firstColumn >3?3:2,false);
+            //合并指定范围行的单元格
+            FilePrintUtils.mergeCellsInColumnRange(workbook,40, startRow, endRow, firstColumn, lastColumn);
+
+            //设置第一列列宽
+            FilePrintUtils.setColumnWidth(sheet,0,10);
+            // 保存修改后的Excel文件
+            try (OutputStream outputStream = new FileOutputStream(filePath)) {
+                workbook.write(outputStream);
+            }
+        }
+    }
+
+
     @NotNull
     private Map<String, Object> getSignImageMap(PrintPatrolTaskDTO taskDTO) {
         Map<String, Object> imageMap = MapUtils.newHashMap();
@@ -530,7 +591,7 @@ public class PatrolTaskPrintServiceImpl extends PatrolTaskServiceImpl {
         return imageMap;
     }
 
-    private List<PrintDTO> getPrint(String id, Map<String, Object> map) {
+    private List<PrintDTO> getPrintOthers(String id, Map<String, Object> map) {
         List<PrintDTO> getPrint = new ArrayList<>();
         List<PatrolStationDTO> billGangedInfo = patrolTaskDeviceService.getBillGangedInfo(id);
         for (PatrolStationDTO dto : billGangedInfo) {
@@ -596,6 +657,39 @@ public class PatrolTaskPrintServiceImpl extends PatrolTaskServiceImpl {
         return getPrint;
     }
 
+
+    private List<PrintDTO> getPrint(String id) {
+        List<PrintDTO> getPrint = new ArrayList<>();
+        List<PatrolStationDTO> billGangedInfo = patrolTaskDeviceService.getBillGangedInfo(id);
+        for (PatrolStationDTO dto : billGangedInfo) {
+            //获取检修项
+            List<String> collect = dto.getBillInfo().stream().filter(d -> StrUtil.isNotEmpty(d.getBillCode())).map(t -> t.getBillCode()).collect(Collectors.toList());
+            List<PatrolCheckResultDTO> checkResultList = patrolCheckResultMapper.getCheckByTaskDeviceIdAndParent(collect);
+            for (PatrolCheckResultDTO c : checkResultList) {
+                List<PatrolCheckResultDTO> list = patrolCheckResultMapper.getQualityStandard(collect,c.getOldId());
+                for (PatrolCheckResultDTO t :list){
+                    PrintDTO printDTO = new PrintDTO();
+                    printDTO.setStandard(t.getQualityStandard());
+                    printDTO.setEquipment(c.getContent());
+                    printDTO.setContent(t.getContent());
+                    if(ObjectUtil.isEmpty(t.getCheckResult())){
+                        printDTO.setResultTrue("☐正常");
+                        printDTO.setResultFalse("☐异常");
+                    }else {
+                        printDTO.setResultTrue(t.getCheckResult()==0?"☐正常":"☑正常");
+                        printDTO.setResultFalse(t.getCheckResult()==0?"☑异常":"☐异常");
+                    }
+                    printDTO.setRemark(t.getRemark());
+                    printDTO.setLocation(dto.getStationName());
+                    printDTO.setSubSystem(t.getSubsystemName());
+                    if (ObjectUtil.isNotEmpty(printDTO.getStandard())){
+                        getPrint.add(printDTO);
+                    }
+                }
+            }
+        }
+        return getPrint;
+    }
     private List<PrintDTO> getRemark(String id) {
         List<PrintDTO> getRemark = new ArrayList<>();
         List<PatrolStationDTO> billGangedInfo = patrolTaskDeviceService.getBillGangedInfo(id);
