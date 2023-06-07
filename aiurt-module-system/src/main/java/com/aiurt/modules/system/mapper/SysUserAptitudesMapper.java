47218afb71e9b9b03b303173ebc0db09e10ff5c4
cjb <chenjiabao@aiurt.onaliyun.com>
2023-06-07 10:22:11 +0800

    feat(系统模块): 用户资质、用户绩效entity、mapper等添加

---
+++ aiurt-module-system/src/main/java/com/aiurt/modules/system/mapper/SysUserAptitudesMapper.java
@@ -0,0 +1,17 @@
+package com.aiurt.modules.system.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+import com.aiurt.modules.system.entity.SysUserAptitudes;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * @Description: sys_user_aptitudes
+ * @Author: aiurt
+ * @Date:   2023-06-07
+ * @Version: V1.0
+ */
+public interface SysUserAptitudesMapper extends BaseMapper<SysUserAptitudes> {
+
+}
---
+++ aiurt-module-system/src/main/java/com/aiurt/modules/system/mapper/SysUserPerfMapper.java
@@ -0,0 +1,14 @@
+package com.aiurt.modules.system.mapper;
+
+import com.aiurt.modules.system.entity.SysUserPerf;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * @Description: sys_user_perf
+ * @Author: aiurt
+ * @Date:   2023-06-07
+ * @Version: V1.0
+ */
+public interface SysUserPerfMapper extends BaseMapper<SysUserPerf> {
+
+}
