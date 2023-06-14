9aa70df87a06b58ddc197061e8881c50756072bf
chendaoyue <14767753450@163.com>
2023-06-14 14:59:43 +0800

    feat(故障知识库）删除重复字段

--- aiurt-module-fault/aiurt-module-fault-api/src/main/java/com/aiurt/modules/faultknowledgebase/entity/FaultKnowledgeBase.java
+++ aiurt-module-fault/aiurt-module-fault-api/src/main/java/com/aiurt/modules/faultknowledgebase/entity/FaultKnowledgeBase.java
@@ -144,7 +144,6 @@ public class FaultKnowledgeBase extends DictEntity implements Serializable {
     @ApiModelProperty(value = "状态(0:待审批,1:已审批,2:已驳回)")
     @Dict(dicCode = "fault_status")
     private Integer status;
-
     /**设备类型名称*/
     @Excel(name = "设备类型名称", width = 15)
     @ApiModelProperty(value = "设备类型名称")
