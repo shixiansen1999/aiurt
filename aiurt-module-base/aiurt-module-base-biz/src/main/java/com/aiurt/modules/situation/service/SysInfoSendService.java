d247a2f30f8acf3275237aad430358934b7d4d22
hlq <1317009531@qq.com>
2023-06-19 17:16:16 +0800

    fix（特情）修复特情阅读记录失效

--- aiurt-module-base/aiurt-module-base-biz/src/main/java/com/aiurt/modules/situation/service/SysInfoSendService.java
+++ aiurt-module-base/aiurt-module-base-biz/src/main/java/com/aiurt/modules/situation/service/SysInfoSendService.java
@@ -12,4 +12,9 @@ import com.baomidou.mybatisplus.extension.service.IService;
  */
 public interface SysInfoSendService extends IService<SysAnnouncementSend> {
 
+    /**
+     * 用户告，修改阅读状态
+     * @param sysAnnouncementSend 修改参数
+     */
+    void updateReadFlag(SysAnnouncementSend sysAnnouncementSend);
 }
