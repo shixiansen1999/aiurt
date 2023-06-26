c581d01bb610faa2530a0ce771b1c32150e01ad9
lkj <likangjie1221@163.com>
2023-06-26 19:47:39 +0800

    fix(统计报表bug修改):

--- aiurt-module-system/src/main/java/com/aiurt/modules/subsystem/service/impl/CsSubsystemServiceImpl.java
+++ aiurt-module-system/src/main/java/com/aiurt/modules/subsystem/service/impl/CsSubsystemServiceImpl.java
@@ -29,7 +29,6 @@ import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import liquibase.pro.packaged.S;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.ObjectUtils;
