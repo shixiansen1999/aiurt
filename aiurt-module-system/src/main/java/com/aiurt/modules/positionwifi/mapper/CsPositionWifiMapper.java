a34d3b369ad50d19f3b13aec74cf0d4371f46d17
lkj <likangjie1221@163.com>
2023-06-28 12:13:47 +0800

    fix(故障列表班组维度上周对比数bug修改):

--- aiurt-module-system/src/main/java/com/aiurt/modules/positionwifi/mapper/CsPositionWifiMapper.java
+++ aiurt-module-system/src/main/java/com/aiurt/modules/positionwifi/mapper/CsPositionWifiMapper.java
@@ -3,7 +3,6 @@ package com.aiurt.modules.positionwifi.mapper;
 import com.aiurt.modules.positionwifi.entity.CsPositionWifi;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
 import org.jeecg.common.system.vo.StationAndMacModel;
 
 import java.util.List;
