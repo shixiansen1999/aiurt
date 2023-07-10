6ea1d77950920f309938fe9027ae995a840649ad
wgp <123456@qq.com>
2023-07-10 14:35:27 +0800

    feat(检修标准导入): 增加设备类型校验器和转换器

---
+++ aiurt-module-patrol/aiurt-module-patrol-biz/src/main/java/com/aiurt/boot/standard/handler/DeviceTypeCodeNameConvertHandler.java
@@ -0,0 +1,41 @@
+package com.aiurt.boot.standard.handler;
+
+import cn.hutool.core.util.ObjectUtil;
+import com.aiurt.modules.device.entity.DeviceType;
+import com.aiurt.modules.entity.Column;
+import com.aiurt.modules.handler.IRowDataConvertHandler;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import org.jeecg.common.system.api.ISysBaseAPI;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.util.Map;
+
+/**
+ * @author lkj
+ */
+@Component("DeviceTypeCodeNameConvertHandler")
+public class DeviceTypeCodeNameConvertHandler implements IRowDataConvertHandler {
+
+    @Resource
+    private ISysBaseAPI sysBaseApi;
+
+    @Override
+    public String convert(Column value, Map<String, Column> row) {
+
+        Column majorCodeColumn = row.get("profession_code");
+
+        Column subsystemCodeColumn = row.get("subsystem_code");
+        Object subsystem = subsystemCodeColumn.getData();
+
+        LambdaQueryWrapper<DeviceType> wrapper = new LambdaQueryWrapper<>();
+        wrapper.eq(DeviceType::getMajorCode, (String) majorCodeColumn.getData()).eq(DeviceType::getSystemCode, (String) subsystem);
+
+        // 查询设备类型是否匹配专业和子系统
+        DeviceType csMajorByCodeTypeName = sysBaseApi.getDeviceTypeByCode((String) majorCodeColumn.getData(), (String) subsystem, (String) value.getData());
+        if (ObjectUtil.isNotNull(csMajorByCodeTypeName)) {
+            value.setData(csMajorByCodeTypeName.getCode());
+        }
+        return null;
+    }
+}
