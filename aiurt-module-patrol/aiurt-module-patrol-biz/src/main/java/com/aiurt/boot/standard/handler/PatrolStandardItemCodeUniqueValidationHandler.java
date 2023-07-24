e5075bbb192ae1d844f7e280cd876399f9877caa
hlq <1317009531@qq.com>
2023-07-24 16:35:30 +0800

    fix(巡视-校验检查项编号):修复校验唯一报错问题

--- aiurt-module-patrol/aiurt-module-patrol-biz/src/main/java/com/aiurt/boot/standard/handler/PatrolStandardItemCodeUniqueValidationHandler.java
+++ aiurt-module-patrol/aiurt-module-patrol-biz/src/main/java/com/aiurt/boot/standard/handler/PatrolStandardItemCodeUniqueValidationHandler.java
@@ -33,8 +33,8 @@ public class PatrolStandardItemCodeUniqueValidationHandler implements Validation
         // 获取当前列的值
         Object currentValue = currentColumn.getData();
         if (ObjectUtil.isNotEmpty(currentValue)) {
-            PatrolStandardItems patrolStandardItems = patrolStandardItemsMapper.selectOne(new QueryWrapper<PatrolStandardItems>().lambda().eq(PatrolStandardItems::getCode, currentValue).eq(PatrolStandardItems::getDelFlag, 0));
-            if (ObjectUtil.isNotEmpty(patrolStandardItems)) {
+            Long itemNums = patrolStandardItemsMapper.selectCount(new QueryWrapper<PatrolStandardItems>().lambda().eq(PatrolStandardItems::getCode, currentValue).eq(PatrolStandardItems::getDelFlag, 0));
+            if (itemNums>0) {
                 return new ValidationResult(false, String.format("%s该字段值在数据库中已存在", currentColumn.getName()));
             }
         }
