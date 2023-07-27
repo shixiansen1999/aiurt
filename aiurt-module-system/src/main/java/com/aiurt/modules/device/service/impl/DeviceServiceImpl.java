b1e3c6ec2388e168f7b9e035c52ff82bc6962d0c
lkj <likangjie1221@163.com>
2023-07-27 16:20:25 +0800

    fix(设备导入线路校验修改):

--- aiurt-module-system/src/main/java/com/aiurt/modules/device/service/impl/DeviceServiceImpl.java
+++ aiurt-module-system/src/main/java/com/aiurt/modules/device/service/impl/DeviceServiceImpl.java
@@ -670,7 +670,7 @@ public class DeviceServiceImpl extends ServiceImpl<DeviceMapper, Device> impleme
 		}else {
 			stringBuilder.append("设备位置不能为空，");
 		}
-		if (StrUtil.isNotEmpty(stationCodeName)) {
+		if (StrUtil.isNotEmpty(stationCodeName) && StrUtil.isNotEmpty(device.getLineCode())) {
 			LambdaQueryWrapper<CsStation> csStationWrapper = new LambdaQueryWrapper<>();
 			csStationWrapper.eq(CsStation::getStationName, stationCodeName)
 					.eq(CsStation::getLineCode, device.getLineCode())
@@ -682,7 +682,7 @@ public class DeviceServiceImpl extends ServiceImpl<DeviceMapper, Device> impleme
 				device.setStationCode(one.getStationCode());
 			}
 		}
-		if (StrUtil.isNotEmpty(positionCodeName)) {
+		if (StrUtil.isNotEmpty(positionCodeName)&& StrUtil.isNotEmpty(device.getStationCode())) {
 			LambdaQueryWrapper<CsStationPosition> positionWrapper = new LambdaQueryWrapper<>();
 			positionWrapper.eq(CsStationPosition::getPositionName, positionCodeName).eq(CsStationPosition::getDelFlag, 0);
 			positionWrapper.eq(CsStationPosition::getLineCode, device.getLineCode()).eq(CsStationPosition::getStaionCode, device.getStationCode());
