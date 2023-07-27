1993b9b2a82d39837433b651cea5755895eb027b
hlq <1317009531@qq.com>
2023-07-27 14:47:40 +0800

    fix(定时任务)优化定时任务查询

--- aiurt-module-system/src/main/java/com/aiurt/modules/quartz/controller/QuartzJobController.java
+++ aiurt-module-system/src/main/java/com/aiurt/modules/quartz/controller/QuartzJobController.java
@@ -67,7 +67,7 @@ public class QuartzJobController {
 			@RequestParam(name = "pageSize", defaultValue = "10") Integer pageSize, HttpServletRequest req) {
 		QueryWrapper<QuartzJob> queryWrapper = QueryGenerator.initQueryWrapper(quartzJob, req.getParameterMap());
 		Page<QuartzJob> page = new Page<QuartzJob>(pageNo, pageSize);
-		queryWrapper.and(wrapper->wrapper.ne("filter_status",1).or().isNull("filter_status"));
+		queryWrapper.lambda().and(wrapper->wrapper.ne(QuartzJob::getFilterStatus,1).or().isNull(QuartzJob::getFilterStatus));
 		IPage<QuartzJob> pageList = quartzJobService.page(page, queryWrapper);
         return Result.ok(pageList);
 
