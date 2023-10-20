76e64c2028f32498976230fbf73735905c887bc6
GAOWEI\gaowei <gaoweif@outlook.com>
2023-10-20 10:50:29 +0800

    fix(流程) 代码优化异味

--- aiurt-module-flowable/aiurt-module-flowable-biz/src/main/java/com/aiurt/modules/deduplicate/handler/MultiInstanceRuleVerifyHandler.java
+++ aiurt-module-flowable/aiurt-module-flowable-biz/src/main/java/com/aiurt/modules/deduplicate/handler/MultiInstanceRuleVerifyHandler.java
@@ -3,10 +3,8 @@ package com.aiurt.modules.deduplicate.handler;
 import com.aiurt.modules.common.pipeline.AbstractFlowHandler;
 import com.aiurt.modules.deduplicate.context.FlowDeduplicateContext;
 import lombok.extern.slf4j.Slf4j;
-import org.flowable.engine.TaskService;
 import org.flowable.engine.impl.persistence.entity.ExecutionEntity;
 import org.flowable.task.api.Task;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.Objects;
@@ -21,8 +19,7 @@ public class MultiInstanceRuleVerifyHandler<T extends FlowDeduplicateContext> ex
 
     private static final String IS_MUlTI_ASSIGN_TASK = "is_multi_assign_task";
 
-    @Autowired
-    private TaskService taskService;
+
 
     /**
      * 执行任务
@@ -49,7 +46,7 @@ public class MultiInstanceRuleVerifyHandler<T extends FlowDeduplicateContext> ex
         }
 
         // 加签用户
-        if (Objects.nonNull(isMultiAssignTask) && isMultiAssignTask) {
+        if (isMultiAssignTask) {
             context.setContinueChain(false);
             if (log.isDebugEnabled()) {
                 log.debug("审批去重，加签规则校验，该用户是加签任务， 审批去重不生效，任务id：{}， 节点id：{}", task.getId(), task.getTaskDefinitionKey());
