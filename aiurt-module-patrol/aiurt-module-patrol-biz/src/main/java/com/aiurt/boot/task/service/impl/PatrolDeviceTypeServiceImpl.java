5b9b73c0945803e03328f390bc18cdca6cfd7f5f
sbx <26050364543@qq.com>
2023-11-07 09:54:51 +0800

    feat(巡视): 保存巡视任务标准关联设备类型

---
+++ aiurt-module-patrol/aiurt-module-patrol-biz/src/main/java/com/aiurt/boot/task/service/impl/PatrolDeviceTypeServiceImpl.java
@@ -0,0 +1,16 @@
+package com.aiurt.boot.task.service.impl;
+
+import com.aiurt.boot.task.entity.PatrolDeviceType;
+import com.aiurt.boot.task.mapper.PatrolDeviceTypeMapper;
+import com.aiurt.boot.task.service.IPatrolDeviceTypeService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author sbx
+ * @since 2023/11/6
+ */
+@Service
+public class PatrolDeviceTypeServiceImpl extends ServiceImpl<PatrolDeviceTypeMapper, PatrolDeviceType> implements IPatrolDeviceTypeService {
+
+}
